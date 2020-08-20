78340d17031c86b0d9439b3ed9d24196fd5570b7
Brian Kroth <bpkroth@microsoft.com>
2020-08-20 16:34:16 -0500

    add a bash equivalent for the grpc generation cmd while we're at it

--- source/Mlos.Proto/GeneratePythonGrpcCode.cmd
+++ source/Mlos.Proto/GeneratePythonGrpcCode.cmd
@@ -4,7 +4,14 @@
 @echo off
 rem We need to recreate the structure of the python module in the input to grpc
 rem to have the imports be generated correctly. See https://github.com/grpc/grpc/issues/9575#issuecomment-293934506
+
+rem Start in the script directory.
+pushd "%~dp0"
+
 mkdir mlos\Grpc
 copy OptimizerService.proto mlos\Grpc\
 python -m grpc_tools.protoc -I . --python_out=..\Mlos.Python --grpc_python_out=..\Mlos.Python  mlos\Grpc\OptimizerService.proto
 rd /s /q mlos
+
+popd
+@echo on
---
+++ source/Mlos.Proto/GeneratePythonGrpcCode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# We need to recreate the structure of the python module in the input to grpc
+# to have the imports be generated correctly.
+# See https://github.com/grpc/grpc/issues/9575#issuecomment-293934506
+
+set -eu
+
+# Start in the script directory.
+scriptdir=$(readlink -f "$(dirname "$0")")
+cd "$scriptdir"
+
+mkdir -p mlos/Grpc
+cp OptimizerService.proto mlos/Grpc/
+python3 -m grpc_tools.protoc -I . --python_out=../Mlos.Python --grpc_python_out=../Mlos.Python  mlos/Grpc/OptimizerService.proto
+rm -rf mlos
