5d3c5654200ad5c5215056f819173e164fc87abd
Brian Kroth <bpkroth@microsoft.com>
2020-08-20 11:13:37 -0500

    add the build script referenced in the github actions ci pipeline

---
+++ scripts/build.windows.cmd
@@ -0,0 +1,17 @@
+@echo off
+rem Build script for Windows.  Used in Github Actions for continuous integration checks.
+
+rem Move to the root of the repo.
+pushd "%~dp0\.."
+
+rem Setup the build environment.
+call .\scripts\init.windows.cmd
+
+rem Make style check failures fail the build.
+set UncrustifyAutoFix=false
+
+rem Build all .vcxproj and .csproj files listed in the various dirs.proj files.
+rem Note: This also includes running unit tests.
+msbuild /m /r dirs.proj
+
+exit /B %ERRORLEVEL%
