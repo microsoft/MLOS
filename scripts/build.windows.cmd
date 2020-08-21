964c3021671a986985d248c6085153bdfc3eb1d2
bpkroth <bpkroth@users.noreply.github.com>
2020-08-21 15:00:41 -0500

    Create initial github actions pipeline for building on windows (#11)
    * Create initial github actions pipeline for building on windows
    
    * add the build script referenced in the github actions ci pipeline
    
    * comments
    
    * commit something that should fail the uncrustify check just to make sure that the pipelines fail
    
    * Revert "commit something that should fail the uncrustify check just to make sure that the pipelines fail"
    
    This reverts commit 3fee0799f0ab43503ee2db7287457081e8f7b288.
    
    Manually verified that this does indeed fail the build pipeline.
    
    * tell editors to use 2 spaces instead of 4 by default for yaml files

---
+++ scripts/build.windows.cmd
@@ -0,0 +1,18 @@
+@echo off
+rem Build script for Windows.  Used in Github Actions for continuous integration checks.
+rem Note: "set Configuration={Release or Debug}" to switch the build type.
+
+rem Move to the root of the repo.
+pushd "%~dp0\.."
+
+rem Setup the build environment.
+call .\scripts\init.windows.cmd
+
+rem Make style check failures fail the build.
+set UncrustifyAutoFix=false
+
+rem Build all .vcxproj and .csproj files listed in the various dirs.proj files.
+rem Note: This also includes running unit tests.
+msbuild /m /r dirs.proj
+
+exit /B %ERRORLEVEL%
