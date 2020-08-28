f3cf9bf3efad2aac8440d96b5f7b6198dcf99586
Andreas C Mueller <andreasmuellerml@gmail.com>
2020-08-28 14:58:51 -0400

    add code to download book theme to build_site.sh

--- website/build_site.sh
+++ website/build_site.sh
@@ -4,3 +4,5 @@ cp -r ../documentation content/
 jupyter nbconvert ../source/Mlos.Notebooks/*.ipynb --to markdown --output-dir content/notebooks
 
 cp ../README.md content/_index.md
+
+git clone --depth 1 https://github.com/alex-shpak/hugo-book.git themes/book/
