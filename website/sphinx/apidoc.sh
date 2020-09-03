109abae7b974b907aac4ed38ed4674a732ba5747
Andreas C Mueller <andreasmuellerml@gmail.com>
2020-09-03 17:07:37 -0400

    make a change

--- website/sphinx/apidoc.sh
+++ website/sphinx/apidoc.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
+
+# ensure we're in the right folder
+scriptdir=$(readlink -f "$(dirname "$0")")
+cd "$scriptdir/.."
+cd website/sphinx
+
+echo "Generating Python API rst files"
+
 sphinx-apidoc -o api -t _templates ../../source/Mlos.Python/mlos ../../source/Mlos.Python/mlos/unit_tests/* -d 1 -f -e
