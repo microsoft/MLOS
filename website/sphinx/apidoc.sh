89a246641dc1234d8a32092374472bbddf3412cc
Brian Kroth <bpkroth@microsoft.com>
2020-09-15 21:27:47 -0500

    document an issue I ran into

--- website/sphinx/apidoc.sh
+++ website/sphinx/apidoc.sh
@@ -21,6 +21,9 @@ echo "Installing dependencies for generating Python API docs"
 $pythonCmd -m pip install -e $MLOS_ROOT/source/Mlos.Python/
 $pythonCmd -m pip install sphinx sphinx_rtd_theme numpydoc matplotlib
 
+# Work around some issues with python not picking up the system installed package sometimes.
+$pythonCmd -m pip install 'kiwisolver>=1.0.2'
+
 echo "Generating Python API rst files"
 
 sphinx-apidoc -o api -t _templates $MLOS_ROOT/source/Mlos.Python/mlos $MLOS_ROOT/source/Mlos.Python/mlos/unit_tests/* -d 1 -f -e
