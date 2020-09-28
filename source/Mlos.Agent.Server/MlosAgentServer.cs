01f497d4a09f6798483c3db95dfa5e435f6a0b1f
Brian Kroth <bpkroth@microsoft.com>
2020-09-28 14:45:51 -0500

    look for extra args and output more useful help text

--- source/Mlos.Agent.Server/MlosAgentServer.cs
+++ source/Mlos.Agent.Server/MlosAgentServer.cs
@@ -7,11 +7,14 @@
 // -----------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 using CommandLine;
+using CommandLine.Text;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
@@ -57,20 +60,29 @@ namespace Mlos.Agent.Server
         {
             string executableFilePath = null;
             Uri optimizerAddressUri = null;
+            IEnumerable<string> extraArgs = null;
 
-            var cliOptsParseResult = CommandLine.Parser.Default.ParseArguments<CliOptions>(args)
+            var cliOptsParser = new Parser(with => with.HelpWriter = null);
+            var cliOptsParseResult = cliOptsParser.ParseArguments<CliOptions>(args)
                 .WithParsed(parsedOptions =>
                 {
                     executableFilePath = parsedOptions.Executable;
                     optimizerAddressUri = parsedOptions.OptimizerUri;
+                    extraArgs = parsedOptions.ExtraArgs;
                 });
             if (cliOptsParseResult.Tag == ParserResultType.NotParsed)
             {
-                // CommandLine already prints the help text for us in this case.
-                //
-                Console.Error.WriteLine("Failed to parse command line options.");
-                Environment.Exit(1);
+                cliOptsParseResult.WithNotParsed(errs => ShowUsageHelp(
+                    cliOptsParseResult,
+                    errors: errs,
+                    msg: "Failed to parse command line options."));
             }
+            else if (extraArgs != null && extraArgs.Any())
+            {
+                ShowUsageHelp(cliOptsParseResult, msg: "Extra arguments found.");
+            }
+
+            cliOptsParser.Dispose();
 
             // Check for the executable before setting up any shared memory to
             // reduce cleanup issues.
@@ -220,6 +232,56 @@ namespace Mlos.Agent.Server
             Console.WriteLine("Mlos.Agent exited.");
         }
 
+        private static void ShowUsageHelp<T>(ParserResult<T> parserResult, IEnumerable<Error> errors = null, string msg = null)
+        {
+            if (msg != null)
+            {
+                Console.Error.WriteLine(msg);
+                Console.Error.WriteLine(string.Empty);
+            }
+
+            if (errors == null)
+            {
+                errors = new List<Error>();
+            }
+
+            HelpText helpText = null;
+            if (errors.IsVersion())
+            {
+                helpText = HelpText.AutoBuild(parserResult);
+            }
+            else
+            {
+                helpText = HelpText.AutoBuild(
+                    parserResult,
+                    onError: ht =>
+                    {
+                        ht.AddNewLineBetweenHelpSections = true;
+                        ht.AddPreOptionsLines(new[]
+                        {
+                            "usage mode 1:  Wait for an application to register over global shared memory, without an optimizer.",
+                            "    dotnet Mlos.Agent.Server.dll",
+                            string.Empty,
+                            "usage mode 2:  Wait for an application to register over global shared memory, and prepare to communicate with an MLOS optimizer listening at the given Grpc URI.",
+                            "    dotnet Mlos.Agent.Server.dll --optimizer-uri http://localhost:50051",
+                            string.Empty,
+                            "usage mode 3:  Start an executable to communicate over freshly prepared global shared memory.",
+                            "    dotnet Mlos.Agent.Server.dll --executable path/to/executable",
+                            string.Empty,
+                            "usage mode 4:  Start an executable to communicate over freshly prepared global shared memory and prepare to communicate with an MLOS optimizer listening at the given Grpc URI.",
+                            "    dotnet Mlos.Agent.Server.dll --executable path/to/executable --optimizer-uri http://localhost:50051",
+                            string.Empty,
+                            "Note: the optimizer service can be started using the start_optimizer_microservice command from the mlos Python module.",
+                        });
+                        return HelpText.DefaultParsingErrorsHandler(parserResult, ht);
+                    },
+                    e => e);
+            }
+
+            Console.WriteLine(helpText);
+            Environment.Exit(1);
+        }
+
         /// <summary>
         /// The command line options for this application.
         /// </summary>
@@ -230,6 +292,13 @@ namespace Mlos.Agent.Server
 
             [Option("optimizer-uri", Required = false, Default = null, HelpText = "A URI to connect to the MLOS Optimizer service over GRPC (e.g. 'http://localhost:50051').")]
             public Uri OptimizerUri { get; set; }
+
+            /// <remarks>
+            /// Just used to detect any extra arguments so we can throw a warning.
+            /// See Also: https://github.com/microsoft/MLOS/issues/112.
+            /// </remarks>
+            [CommandLine.Value(0)]
+            public IEnumerable<string> ExtraArgs { get; set; }
         }
     }
 }
