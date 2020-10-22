7c0e1fc1a280611d609b17ba544eb2cd7795c5ef
edcthayer <edcthayer@gmail.com>
2020-10-22 14:09:02 -0700

    Adding OneHotEncoder HypergridAdapter (#130)
    * initial commit for OneHotEncoder HypergridAdapter
    
    * Cleaned up project/unproject; dropped point project/unproject methods; corrected some defects in Point, HypergridAdapter; Added more unit tests
    
    * Addressing PR feedback
    
    * Addressing pylint
    
    * Addressing PR feedback
    
    Co-authored-by: Ed Thayer <edthaye@microsoft.com>

--- source/Mlos.Python/mlos/Spaces/HypergridAdapters/__init__.py
+++ source/Mlos.Python/mlos/Spaces/HypergridAdapters/__init__.py
@@ -3,12 +3,14 @@
 # Licensed under the MIT License.
 #
 from .CategoricalToDiscreteHypergridAdapter import CategoricalToDiscreteHypergridAdapter
+from .CategoricalToOneHotEncodedHypergridAdapter import CategoricalToOneHotEncodedHypergridAdapter
 from .DiscreteToUnitContinuousHypergridAdapter import DiscreteToUnitContinuousHypergridAdapter
 from .HierarchicalToFlatHypergridAdapter import HierarchicalToFlatHypergridAdapter
 from .HypergridAdapter import HypergridAdapter
 
 __all__ = [
     "CategoricalToDiscreteHypergridAdapter",
+    "CategoricalToOneHotEncodedHypergridAdapter",
     "DiscreteToUnitContinuousHypergridAdapter",
     "HierarchicalToFlatHypergridAdapter",
     "HypergridAdapter",
