fc0dba077aa53116e7e3ff4a39bde6dc370a6d8f
Andreas C Mueller <andreasmuellerml@gmail.com>
2020-10-30 17:28:19 -0700

    use pytest to run python tests

--- scripts/run-python-tests.sh
+++ scripts/run-python-tests.sh
@@ -10,9 +10,4 @@ set -eu
 scriptdir=$(readlink -f "$(dirname "$0")")
 cd "$scriptdir/.."
 
-. "$scriptdir/util.sh"
-pythoncmd=`getPythonCmd`
-
-# Linux filesystems are case-sensitive, so we need to tell the python unit test
-# scanner to look for upper-case files as well:
-$pythoncmd -m unittest discover --verbose --locals --failfast -s source/Mlos.Python -p "[Tt]est*.py" $*
+pytest -svx source\Mlos.Python
