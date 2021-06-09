b7eda41c2fa580e9dd4dfdf196ee638de55d8e7d
Brian Kroth <bpkroth@microsoft.com>
2021-06-08 22:34:31 -0500

    fixups

--- .github/workflows/gen-workflows.sh
+++ .github/workflows/gen-workflows.sh
@@ -7,7 +7,17 @@ cd "$scriptdir"
 
 # Prepare the common rules for each version using the template.
 for UbuntuVersion in {16,18,20}.04; do
-    cat ./ubuntu.yml.tmpl | sed "s/%UBUNTU_VERSION%/$UbuntuVersion/g" > ./ubuntu-$UbuntuVersion.yml
+    if [ "$UbuntuVersion" == '20.04' ]; then
+        DockerPublishJobNeeds='[prep-vars, docker-image-fresh-build, docker-image-cached-build, linux-build-test, linux-python-checks, build-publish-website]'
+    else
+        DockerPublishJobNeeds='[prep-vars, docker-image-fresh-build, docker-image-cached-build, linux-build-test]'
+    fi
+
+    cp ./ubuntu.yml.tmpl ./ubuntu-$UbuntuVersion.yml
+    sed -i \
+        -e "s/%UBUNTU_VERSION%/$UbuntuVersion/g" \
+        -e "s/%DockerPublishJobNeeds%/$DockerPublishJobNeeds/g" \
+        ./ubuntu-$UbuntuVersion.yml
 done
 # Append the extra rules to that file.
 grep -q '^[ ]*#%EXTRA_RULES%#$' ./ubuntu-20.04.yml
