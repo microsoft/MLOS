44f3598ae7a28044b70ec9ab6df5db5c97de0e5c
Zachary Porter <t-zporter@microsoft.com>
2021-06-21 21:55:57 -0700

    Reroll

--- source/Mlos.Proto/GeneratePythonGrpcCode.sh
+++ source/Mlos.Proto/GeneratePythonGrpcCode.sh
@@ -2,13 +2,14 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+#
 # We need to recreate the structure of the python module in the input to grpc
 # to have the imports be generated correctly.
 # See https://github.com/grpc/grpc/issues/9575#issuecomment-293934506
 #
 # Note: We currently do not use imports in order to keep OptimizerMonitoringService
 # and OptimizerService seperate for future iterations (one may lead the other)
-
+#
 
 
 set -eu
