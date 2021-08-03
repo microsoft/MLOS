d9827e6cdc5fac8e2315a6e2636f8512755e200c
Zachary Porter <t-zporter@microsoft.com>
2021-08-03 15:41:13 -0700

    Now capable of calculating values of series. No probabilistic valuations yet

--- source/Mlos.Python/mlos/Optimizers/ExperimentDesigner/ExperimentDesigner.py
+++ source/Mlos.Python/mlos/Optimizers/ExperimentDesigner/ExperimentDesigner.py
@@ -18,6 +18,7 @@ from .UtilityFunctionOptimizers.GlowWormSwarmOptimizer import GlowWormSwarmOptim
 from .UtilityFunctions.ConfidenceBoundUtilityFunction import ConfidenceBoundUtilityFunction, confidence_bound_utility_function_config_store
 from .UtilityFunctions.MultiObjectiveProbabilityOfImprovementUtilityFunction import MultiObjectiveProbabilityOfImprovementUtilityFunction, \
     multi_objective_probability_of_improvement_utility_function_config_store
+from .UtilityFunctions.SeriesDifferenceUtilityFunction import SeriesDifferenceUtilityFunction, series_difference_utility_function_config_store
 
 from .UtilityFunctionOptimizers.UtilityFunctionOptimizerFactory import UtilityFunctionOptimizerFactory
 
@@ -28,7 +29,8 @@ experiment_designer_config_store = ComponentConfigStore(
         dimensions=[
             CategoricalDimension('utility_function_implementation', values=[
                 ConfidenceBoundUtilityFunction.__name__,
-                MultiObjectiveProbabilityOfImprovementUtilityFunction.__name__
+                MultiObjectiveProbabilityOfImprovementUtilityFunction.__name__,
+                SeriesDifferenceUtilityFunction.__name__
             ]),
             CategoricalDimension('numeric_optimizer_implementation', values=[
                 RandomSearchOptimizer.__name__,
@@ -43,6 +45,9 @@ experiment_designer_config_store = ComponentConfigStore(
     ).join(
         subgrid=multi_objective_probability_of_improvement_utility_function_config_store.parameter_space,
         on_external_dimension=CategoricalDimension('utility_function_implementation', values=[MultiObjectiveProbabilityOfImprovementUtilityFunction.__name__])
+    ).join(
+        subgrid=series_difference_utility_function_config_store.parameter_space,
+        on_external_dimension=CategoricalDimension('utility_function_implementation', values=[SeriesDifferenceUtilityFunction.__name__])
     ).join(
         subgrid=random_search_optimizer_config_store.parameter_space,
         on_external_dimension=CategoricalDimension('numeric_optimizer_implementation', values=[RandomSearchOptimizer.__name__])
@@ -137,6 +142,7 @@ class ExperimentDesigner:
         self.surrogate_model: MultiObjectiveRegressionModel = surrogate_model
         self.rng = np.random.Generator(np.random.PCG64())
 
+        # TODO ZACK: Why objectives[0].minimize?
         if designer_config.utility_function_implementation == ConfidenceBoundUtilityFunction.__name__:
             self.utility_function = ConfidenceBoundUtilityFunction(
                 function_config=self.config.confidence_bound_utility_function_config,
@@ -151,6 +157,13 @@ class ExperimentDesigner:
                 function_config=self.config.multi_objective_probability_of_improvement_config,
                 pareto_frontier=pareto_frontier,
                 surrogate_model=self.surrogate_model,
+                logger=self.logger
+            )
+        elif designer_config.utility_function_implementation == SeriesDifferenceUtilityFunction.__name__:
+            self.utility_function = SeriesDifferenceUtilityFunction(
+                function_config=self.config.confidence_bound_utility_function_config,
+                surrogate_model=self.surrogate_model,
+                objective=self.optimization_problem.objectives[0], # TODO ZACK: I copied this style from ConfidenceBoundUtilityFunction. Not sure about it.
                 logger=self.logger
             )
         else:
---
+++ source/Mlos.Python/mlos/Optimizers/ExperimentDesigner/UtilityFunctions/SeriesDifferenceUtilityFunction.py
@@ -0,0 +1,107 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+import numpy as np
+import pandas as pd
+from scipy.stats import t
+from mlos.Logger import create_logger
+from mlos.Optimizers.OptimizationProblem import SeriesObjective
+from mlos.Optimizers.ExperimentDesigner.UtilityFunctions.UtilityFunction import UtilityFunction
+from mlos.Optimizers.RegressionModels.MultiObjectiveRegressionModel import MultiObjectiveRegressionModel
+from mlos.Optimizers.RegressionModels.Prediction import Prediction
+from mlos.Spaces import SimpleHypergrid, ContinuousDimension, CategoricalDimension, Point
+from mlos.Spaces.Configs.ComponentConfigStore import ComponentConfigStore
+from mlos.Tracer import trace
+
+
+series_difference_utility_function_config_store = ComponentConfigStore(
+    parameter_space=SimpleHypergrid(
+        name="series_difference_utility_function_config",
+        dimensions=[
+            CategoricalDimension(name="utility_function_name", values=["lower_confidence_bound_on_improvement", "upper_confidence_bound_on_improvement"]),
+            ContinuousDimension(name="alpha", min=0.01, max=0.5)
+        ]
+    ),
+    default=Point(
+        utility_function_name="upper_confidence_bound_on_improvement",
+        alpha=0.01
+    )
+)
+
+
+class SeriesDifferenceUtilityFunction(UtilityFunction):
+    def __init__(self, function_config: Point, surrogate_model: MultiObjectiveRegressionModel, objective: SeriesObjective, logger=None):
+        if logger is None:
+            logger = create_logger(self.__class__.__name__)
+        self.logger = logger
+
+        self.config = function_config
+        self.series_objective = objective
+        self._sign = 1 if not self.series_objective.minimize else -1
+        self.surrogate_model: MultiObjectiveRegressionModel = surrogate_model
+
+    @trace()
+    def __call__(self, feature_values_pandas_frame):
+        self.logger.debug(f"Computing utility values for {len(feature_values_pandas_frame.index)} points.")
+
+        predicted_value_col = Prediction.LegalColumnNames.PREDICTED_VALUE.value
+        predicted_value_var_col = Prediction.LegalColumnNames.PREDICTED_VALUE_VARIANCE.value
+        dof_col = Prediction.LegalColumnNames.PREDICTED_VALUE_DEGREES_OF_FREEDOM.value
+        print("Gamma")
+        print(feature_values_pandas_frame)
+
+
+        del feature_values_pandas_frame[f"context_space.{self.series_objective.series_modulation_dimension.name}"]
+        series_vals_df = pd.DataFrame({
+            f"context_space.{self.series_objective.series_modulation_dimension.name}": self.series_objective.series_modulation_dimension.linspace()
+        })
+        feature_values_pandas_frame = feature_values_pandas_frame.merge(series_vals_df, how="cross")
+        print(feature_values_pandas_frame)
+        multi_objective_predictions = self.surrogate_model.predict(features_df=feature_values_pandas_frame)
+
+
+
+
+        # While the models can predict multiple objectives, here we just compute the utility for the first one. Next-steps include:
+        #   1. Select the objective by name
+        #   2. Write multi-objective utility functions
+        #
+        # But for now, the behavior below keeps the behavior of the optimizer unchanged.
+        #
+        predictions = multi_objective_predictions[0]
+        predictions_df = predictions.get_dataframe()
+        print("result")
+        print(self.series_objective.series_valuation_function)
+        print(self.series_objective.series_valuation_function([1,4,9]))
+        print(predictions_df)
+        # TODO ZACK: Vectorize this. I'm sure pandas/numpy can do this faster than I can. @Adam, do you know how?
+        resulting_series_arry = []
+        current_arry = []
+        for index, prediction in predictions_df.iterrows():
+            current_arry.append(prediction[predicted_value_col])
+            if ((index + 1) % len(self.series_objective.series_modulation_dimension)) == 0:
+                resulting_series_arry.append(current_arry)
+                current_arry=[]
+
+
+        series_valuations = []
+        for series in resulting_series_arry:
+            series_valuations.append(self.series_objective.series_valuation_function(series))
+
+        print(len(series_valuations))
+        print(series_valuations[0])
+        t_values = t.ppf(1 - self.config.alpha / 2.0, predictions_df[dof_col])
+        confidence_interval_radii = t_values * np.sqrt(predictions_df[predicted_value_var_col])
+
+        if self.config.utility_function_name == "lower_confidence_bound_on_improvement":
+            utility_function_values = predictions_df[predicted_value_col] * self._sign - confidence_interval_radii
+        elif self.config.utility_function_name == "upper_confidence_bound_on_improvement":
+            utility_function_values = predictions_df[predicted_value_col] * self._sign + confidence_interval_radii
+        else:
+            raise RuntimeError(f"Invalid utility function name: {self.config.utility_function_name}.")
+
+        utility_function_values = pd.to_numeric(arg=utility_function_values, errors='raise')
+        utility_df = pd.DataFrame(data=utility_function_values, index=predictions_df.index, columns=['utility'], dtype='float')
+        assert utility_df.dtypes['utility'] == float, f"{utility_df} has the wrong type for the 'utility' column: {utility_df.dtypes['utility']}"
+        return utility_df
