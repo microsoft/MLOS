350145d71b20d27ff465bd6198bad09acfd20804
Brian Kroth <Brian.Kroth@microsoft.com>
2022-03-15 17:47:04 +0000

    Merged PR 377: Add a workaround to let emukit unit test run
    Add a workaround to let emukit unit test run
    
    avoids a poor DISPLAY environment variable in WSL setup interaction with default matplotlib backend

--- mlos_core/optimizers/test/optimizer_test.py
+++ mlos_core/optimizers/test/optimizer_test.py
@@ -2,6 +2,9 @@
 Tests for Bayesian Optimizers.
 """
 
+import os
+from warnings import warn
+
 from typing import Type
 
 import pytest
@@ -11,8 +14,7 @@ import ConfigSpace as CS
 from mlos_core.optimizers import BaseOptimizer, EmukitOptimizer, SkoptOptimizer, RandomOptimizer
 
 @pytest.mark.parametrize(('optimizer_class', 'kwargs'), [
-    # FIXME: hangs on emukit import
-    #(EmukitOptimizer, {}),
+    (EmukitOptimizer, {}),
     (SkoptOptimizer, {'base_estimator': 'gp'}),
     (RandomOptimizer, {})
 ])
@@ -20,6 +22,13 @@ def test_create_optimizer_and_suggest(optimizer_class: Type[BaseOptimizer], kwar
     """
     Helper method for testing optimizers.
     """
+
+    if os.environ.get('DISPLAY', None):
+        import matplotlib
+        matplotlib.rcParams['backend'] = 'agg'
+        warn(UserWarning('DISPLAY environment variable is set, which can cause problems in some setups (e.g. WSL). ' \
+            + f'Adjusting matplotlib backend to "{matplotlib.rcParams["backend"]}" to compensate.'))
+
     # Start defining a ConfigurationSpace for the Optimizer to search.
     input_space = CS.ConfigurationSpace(seed=1234)
 
