937e304c7b1a797258054c5d73d727fb1fc42127
Sergiy Matusevych <sergiym@microsoft.com>
2023-03-09 14:05:11 -0800

    extract prepare_class_load() function; move load_optimizer() to the optimizer package

--- mlos_bench/mlos_bench/optimizer/__init__.py
+++ mlos_bench/mlos_bench/optimizer/__init__.py
@@ -2,12 +2,44 @@
 Interfaces and wrapper classes for optimizers to be used in Autotune.
 """
 
+import logging
+
+from mlos_bench.util import prepare_class_load
+from mlos_bench.environment.tunable import TunableGroups
+
 from mlos_bench.optimizer.base_optimizer import Optimizer
 from mlos_bench.optimizer.mock_optimizer import MockOptimizer
 from mlos_bench.optimizer.mlos_core_optimizer import MlosCoreOptimizer
+
+_LOG = logging.getLogger(__name__)
 
 __all__ = [
     'Optimizer',
     'MockOptimizer',
     'MlosCoreOptimizer',
 ]
+
+
+def load_optimizer(tunables: TunableGroups,
+                   config: dict, global_config: dict = None) -> Optimizer:
+    """
+    Instantiate the Optimizer shim from the configuration.
+
+    Parameters
+    ----------
+    tunables : TunableGroups
+        Tunable parameters of the environment.
+    config : dict
+        Configuration of the optimizer.
+    global_config : dict
+        Global configuration parameters (optional).
+
+    Returns
+    -------
+    opt : Optimizer
+        A new Optimizer instance.
+    """
+    (class_name, opt_config) = prepare_class_load(config, global_config)
+    opt = Optimizer.new(class_name, tunables, opt_config)
+    _LOG.info("Created optimizer: %s", opt)
+    return opt
--- mlos_bench/mlos_bench/run_opt.py
+++ mlos_bench/mlos_bench/run_opt.py
@@ -5,12 +5,11 @@ OS Autotune main optimization loop.
 See `--help` output for details.
 """
 
-import json
 import logging
 
 from mlos_bench.launcher import Launcher
-from mlos_bench.optimizer import Optimizer
-from mlos_bench.environment import Status, Environment, TunableGroups
+from mlos_bench.optimizer import Optimizer, load_optimizer
+from mlos_bench.environment import Status, Environment
 
 _LOG = logging.getLogger(__name__)
 
@@ -29,69 +28,36 @@ def _main():
 
     args = launcher.parse_args()
 
-    config = launcher.global_config
+    global_config = launcher.global_config
     env = launcher.load_env()
 
-    opt = _load_optimizer(
-        env.tunable_params(), launcher.load_config(args.optimizer), config)
+    opt = load_optimizer(
+        env.tunable_params(), launcher.load_config(args.optimizer), global_config)
 
-    result = _optimize(config["experimentId"], env, opt)
+    db = None
+
+    result = _optimize(env, opt, db, global_config["experimentId"])
     _LOG.info("Final result: %s", result)
 
     if not args.no_teardown:
         env.teardown()
 
 
-def _load_optimizer(tunables: TunableGroups, config: dict, global_config: dict) -> Optimizer:
-    """
-    Instantiate the Optimizer shim from the configuration.
-
-    Parameters
-    ----------
-    tunables : TunableGroups
-        Tunable parameters of the environment.
-    config : dict
-        Configuration of the optimizer.
-    global_config : dict
-        Global configuration parameters.
-
-    Returns
-    -------
-    opt : Optimizer
-        A new Optimizer instance.
-    """
-    class_name = config["class"]
-    opt_config = config.setdefault("config", {})
-
-    for key in set(opt_config).intersection(global_config or {}):
-        opt_config[key] = global_config[key]
-
-    if _LOG.isEnabledFor(logging.DEBUG):
-        _LOG.debug("Creating optimizer: %s with config:\n%s",
-                   class_name, json.dumps(opt_config, indent=2))
-
-    opt = Optimizer.new(class_name, tunables, opt_config)
-
-    _LOG.info("Created optimizer: %s", opt)
-    return opt
-
-
-def _optimize(experiment_id: str, env: Environment, opt: Optimizer):
+def _optimize(env: Environment, opt: Optimizer, db,
+              experiment_id: str, run_id: int = 0):
     """
     Main optimization loop.
     """
     _LOG.info("Experiment: %s Env: %s Optimizer: %s", experiment_id, env, opt)
 
-    # Somehow connect to the persistence service using the global parameters
-    db = None
-
-    # Get records of (tunables, status, score) (?) from the previous runs
-    # of the same experiment (or several experiments).
-    # Q: How to restore the telemetry and specify the optimization target?
-    (run_id, tunables_data) = db.restore(experiment_id)
+    # Get records of (tunables, status, score) from the previous runs
+    # of the same experiment (or several compatible experiments).
+    (last_run_id, tunables_data) = db.restore(experiment_id)
     opt.update(tunables_data)
-    # ...can do it more than once for multiple experiments
 
+    run_id = last_run_id or run_id
+
+    # TODO: Restore the telemetry and the optimization target.
     opt_target = 'score'
     opt_direction = 'min'
 
