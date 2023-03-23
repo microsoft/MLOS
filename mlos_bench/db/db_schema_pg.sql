b6de80c4312cc85a160d4bc8e85a3b29405a9ac9
Sergiy Matusevych <sergiym@microsoft.com>
2023-03-23 13:42:56 -0700

    add SQLite3 version of the DB schema; cosmetic fixes to pgsql one

--- mlos_bench/db/db_schema_pg.sql
+++ mlos_bench/db/db_schema_pg.sql
@@ -16,9 +16,9 @@ DROP TYPE IF EXISTS trial_id_t CASCADE;
 DROP TYPE IF EXISTS param_id_t CASCADE;
 DROP TYPE IF EXISTS status_t CASCADE;
 
-CREATE DOMAIN experiment_id_t AS varchar(128) NOT NULL;
-CREATE DOMAIN trial_id_t AS integer NOT NULL;
-CREATE DOMAIN param_id_t AS varchar(256) NOT NULL;
+CREATE DOMAIN experiment_id_t AS VARCHAR(128) NOT NULL;
+CREATE DOMAIN trial_id_t AS INTEGER NOT NULL;
+CREATE DOMAIN param_id_t AS VARCHAR(256) NOT NULL;
 
 -- Should match the `mlos_bench.environment.Status` enum.
 CREATE TYPE status_t AS ENUM (
@@ -34,9 +34,9 @@ CREATE TYPE status_t AS ENUM (
 
 CREATE TABLE experiment_config (
     exp_id experiment_id_t,
-    descr text,
-    git_repo varchar(255) NOT NULL,
-    git_commit varchar(40) NOT NULL,
+    descr TEXT,
+    git_repo VARCHAR(255) NOT NULL,
+    git_commit VARCHAR(40) NOT NULL,
 
     PRIMARY KEY (exp_id)
 );
@@ -46,9 +46,9 @@ CREATE SEQUENCE trial_id_seq;
 CREATE TABLE trial_status (
     exp_id experiment_id_t,
     trial_id trial_id_t DEFAULT nextval('trial_id_seq'),
-    ts_start timestamp NOT NULL DEFAULT now(),
-    ts_end timestamp,
-    status status_t,
+    ts_start TIMESTAMP NOT NULL DEFAULT now(),
+    ts_end TIMESTAMP,
+    trial_status status_t,
 
     PRIMARY KEY (exp_id, trial_id),
     FOREIGN KEY (exp_id) REFERENCES experiment_config(exp_id)
@@ -75,7 +75,7 @@ CREATE TABLE trial_config (
     exp_id experiment_id_t,
     trial_id trial_id_t,
     param_id param_id_t,
-    param_value varchar(255),
+    param_value VARCHAR(255),
 
     PRIMARY KEY (exp_id, trial_id, param_id),
     FOREIGN KEY (exp_id, trial_id) REFERENCES trial_status(exp_id, trial_id),
@@ -86,7 +86,7 @@ CREATE TABLE trial_results (
     exp_id experiment_id_t,
     trial_id trial_id_t,
     param_id param_id_t,
-    param_value varchar(255),
+    param_value VARCHAR(255),
 
     PRIMARY KEY (exp_id, trial_id, param_id),
     FOREIGN KEY (exp_id, trial_id) REFERENCES trial_status(exp_id, trial_id),
@@ -96,9 +96,9 @@ CREATE TABLE trial_results (
 CREATE TABLE trial_telemetry (
     exp_id experiment_id_t,
     trial_id trial_id_t,
-    ts timestamp NOT NULL DEFAULT now(),
+    ts TIMESTAMP NOT NULL DEFAULT now(),
     param_id param_id_t,
-    param_value varchar(255),
+    param_value VARCHAR(255),
 
     UNIQUE (exp_id, trial_id, ts, param_id),
     FOREIGN KEY (exp_id, trial_id) REFERENCES trial_status(exp_id, trial_id),
