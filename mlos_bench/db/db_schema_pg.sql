3f17941abeef5ab192c69bbb83acefa926b08fb9
Sergiy Matusevych <sergiym@microsoft.com>
2023-03-24 13:40:35 -0700

    add SQLite3 and PostgreSQL versions of the DB schema

---
+++ mlos_bench/db/db_schema_pg.sql
@@ -0,0 +1,106 @@
+--
+-- Copyright (c) Microsoft Corporation.
+-- Licensed under the MIT License.
+--
+-- PostgreSQL version of the DB schema.
+
+DROP TABLE IF EXISTS experiment_config CASCADE;
+DROP TABLE IF EXISTS experiment_merge CASCADE;
+DROP TABLE IF EXISTS trial_status CASCADE;
+DROP TABLE IF EXISTS trial_config CASCADE;
+DROP TABLE IF EXISTS trial_results CASCADE;
+DROP TABLE IF EXISTS trial_telemetry CASCADE;
+
+DROP TYPE IF EXISTS experiment_id_t CASCADE;
+DROP TYPE IF EXISTS trial_id_t CASCADE;
+DROP TYPE IF EXISTS param_id_t CASCADE;
+DROP TYPE IF EXISTS status_t CASCADE;
+
+CREATE DOMAIN experiment_id_t AS VARCHAR(128) NOT NULL;
+CREATE DOMAIN trial_id_t AS INTEGER NOT NULL;
+CREATE DOMAIN param_id_t AS VARCHAR(256) NOT NULL;
+
+-- Should match the `mlos_bench.environment.Status` enum.
+CREATE TYPE status_t AS ENUM (
+    'UNKNOWN',
+    'PENDING',
+    'READY',
+    'RUNNING',
+    'SUCCEEDED',
+    'CANCELED',
+    'FAILED',
+    'TIMED_OUT'
+);
+
+CREATE TABLE experiment_config (
+    exp_id experiment_id_t,
+    descr TEXT,
+    git_repo VARCHAR(255) NOT NULL,
+    git_commit VARCHAR(40) NOT NULL,
+
+    PRIMARY KEY (exp_id)
+);
+
+CREATE SEQUENCE trial_id_seq;
+
+CREATE TABLE trial_status (
+    exp_id experiment_id_t,
+    trial_id trial_id_t DEFAULT nextval('trial_id_seq'),
+    ts_start TIMESTAMP NOT NULL DEFAULT now(),
+    ts_end TIMESTAMP,
+    status status_t,
+
+    PRIMARY KEY (exp_id, trial_id),
+    FOREIGN KEY (exp_id) REFERENCES experiment_config(exp_id)
+);
+
+ALTER SEQUENCE trial_id_seq owned by trial_status.trial_id;
+
+CREATE TABLE experiment_merge (
+    dest_exp_id experiment_id_t,
+    dest_trial_id trial_id_t,
+    source_exp_id experiment_id_t,
+    source_trial_id trial_id_t,
+
+    UNIQUE (dest_exp_id, dest_trial_id, source_exp_id, source_trial_id),
+
+    FOREIGN KEY (dest_exp_id, dest_trial_id)
+        REFERENCES trial_status(exp_id, trial_id),
+
+    FOREIGN KEY (source_exp_id, source_trial_id)
+        REFERENCES trial_status(exp_id, trial_id)
+);
+
+CREATE TABLE trial_config (
+    exp_id experiment_id_t,
+    trial_id trial_id_t,
+    param_id param_id_t,
+    param_value VARCHAR(255),
+
+    PRIMARY KEY (exp_id, trial_id, param_id),
+    FOREIGN KEY (exp_id, trial_id) REFERENCES trial_status(exp_id, trial_id),
+    FOREIGN KEY (exp_id) REFERENCES experiment_config(exp_id)
+);
+
+CREATE TABLE trial_results (
+    exp_id experiment_id_t,
+    trial_id trial_id_t,
+    param_id param_id_t,
+    param_value VARCHAR(255),
+
+    PRIMARY KEY (exp_id, trial_id, param_id),
+    FOREIGN KEY (exp_id, trial_id) REFERENCES trial_status(exp_id, trial_id),
+    FOREIGN KEY (exp_id) REFERENCES experiment_config(exp_id)
+);
+
+CREATE TABLE trial_telemetry (
+    exp_id experiment_id_t,
+    trial_id trial_id_t,
+    ts TIMESTAMP NOT NULL DEFAULT now(),
+    param_id param_id_t,
+    param_value VARCHAR(255),
+
+    UNIQUE (exp_id, trial_id, ts, param_id),
+    FOREIGN KEY (exp_id, trial_id) REFERENCES trial_status(exp_id, trial_id),
+    FOREIGN KEY (exp_id) REFERENCES experiment_config(exp_id)
+);
---
+++ mlos_bench/db/db_schema_sqlite.sql
@@ -0,0 +1,82 @@
+--
+-- Copyright (c) Microsoft Corporation.
+-- Licensed under the MIT License.
+--
+-- SQLite3/DuckDB version of the DB schema.
+
+DROP TABLE IF EXISTS experiment_config;
+DROP TABLE IF EXISTS experiment_merge;
+DROP TABLE IF EXISTS trial_status;
+DROP TABLE IF EXISTS trial_config;
+DROP TABLE IF EXISTS trial_results;
+DROP TABLE IF EXISTS trial_telemetry;
+
+CREATE TABLE experiment_config (
+    exp_id VARCHAR(128) NOT NULL,
+    descr TEXT,
+    git_repo VARCHAR(255) NOT NULL,
+    git_commit VARCHAR(40) NOT NULL,
+
+    PRIMARY KEY (exp_id)
+);
+
+CREATE TABLE trial_status (
+    exp_id VARCHAR(128) NOT NULL,
+    trial_id INTEGER NOT NULL,
+    ts_start TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    ts_end TIMESTAMP,
+    -- Should match the text IDs of `mlos_bench.environment.Status` enum:
+    status VARCHAR(16),
+
+    PRIMARY KEY (exp_id, trial_id),
+    FOREIGN KEY (exp_id) REFERENCES experiment_config(exp_id)
+);
+
+CREATE TABLE experiment_merge (
+    dest_exp_id VARCHAR(128) NOT NULL,
+    dest_trial_id INTEGER NOT NULL,
+    source_exp_id VARCHAR(128) NOT NULL,
+    source_trial_id INTEGER NOT NULL,
+
+    UNIQUE (dest_exp_id, dest_trial_id, source_exp_id, source_trial_id),
+
+    FOREIGN KEY (dest_exp_id, dest_trial_id)
+        REFERENCES trial_status(exp_id, trial_id),
+
+    FOREIGN KEY (source_exp_id, source_trial_id)
+        REFERENCES trial_status(exp_id, trial_id)
+);
+
+CREATE TABLE trial_config (
+    exp_id VARCHAR(128) NOT NULL,
+    trial_id INTEGER NOT NULL,
+    param_id VARCHAR(256) NOT NULL,
+    param_value VARCHAR(255),
+
+    PRIMARY KEY (exp_id, trial_id, param_id),
+    FOREIGN KEY (exp_id, trial_id) REFERENCES trial_status(exp_id, trial_id),
+    FOREIGN KEY (exp_id) REFERENCES experiment_config(exp_id)
+);
+
+CREATE TABLE trial_results (
+    exp_id VARCHAR(128) NOT NULL,
+    trial_id INTEGER NOT NULL,
+    param_id VARCHAR(256) NOT NULL,
+    param_value VARCHAR(255),
+
+    PRIMARY KEY (exp_id, trial_id, param_id),
+    FOREIGN KEY (exp_id, trial_id) REFERENCES trial_status(exp_id, trial_id),
+    FOREIGN KEY (exp_id) REFERENCES experiment_config(exp_id)
+);
+
+CREATE TABLE trial_telemetry (
+    exp_id VARCHAR(128) NOT NULL,
+    trial_id INTEGER NOT NULL,
+    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    param_id VARCHAR(256) NOT NULL,
+    param_value VARCHAR(255),
+
+    UNIQUE (exp_id, trial_id, ts, param_id),
+    FOREIGN KEY (exp_id, trial_id) REFERENCES trial_status(exp_id, trial_id),
+    FOREIGN KEY (exp_id) REFERENCES experiment_config(exp_id)
+);
