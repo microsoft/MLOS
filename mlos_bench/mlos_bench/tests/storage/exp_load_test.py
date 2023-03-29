cc4bb28e5a8cfd3ac53d7a6767cf9cb581073918
Sergiy Matusevych <sergiym@microsoft.com>
2023-03-29 14:05:50 -0700

    add more unit tests on storage load/pending functions

--- mlos_bench/mlos_bench/tests/storage/exp_load_test.py
+++ mlos_bench/mlos_bench/tests/storage/exp_load_test.py
@@ -38,6 +38,7 @@ def test_exp_trial_pending(exp_storage_memory_sql: Storage.Experiment,
     trial = exp_storage_memory_sql.trial(tunable_groups)
     (pending,) = list(exp_storage_memory_sql.pending())
     assert pending.trial_id == trial.trial_id
+    assert pending.tunables == tunable_groups
 
 
 def test_exp_trial_pending_2(exp_storage_memory_sql: Storage.Experiment,
@@ -45,8 +46,10 @@ def test_exp_trial_pending_2(exp_storage_memory_sql: Storage.Experiment,
     """
     Start TWO trials and check that both are pending.
     """
-    trial1 = exp_storage_memory_sql.trial(tunable_groups)
-    trial2 = exp_storage_memory_sql.trial(tunable_groups)
+    config1 = tunable_groups.copy().assign({'rootfs': 'ext4'})
+    config2 = tunable_groups.copy().assign({'rootfs': 'ext2'})
+    trial1 = exp_storage_memory_sql.trial(config1)
+    trial2 = exp_storage_memory_sql.trial(config2)
     pending = list(exp_storage_memory_sql.pending())
     assert len(pending) == 2
     assert {pending[0].trial_id, pending[1].trial_id} == {trial1.trial_id, trial2.trial_id}
@@ -77,13 +80,23 @@ def test_exp_trial_success(exp_storage_memory_sql: Storage.Experiment,
 def test_exp_trial_pending_3(exp_storage_memory_sql: Storage.Experiment,
                              tunable_groups: TunableGroups):
     """
-    Start THREE trials, let one succeed, another one fail,
-    and check that one is still pending.
+    Start THREE trials, let one succeed, another one fail and keep one not updated.
+    Check that one is still pending another one can be loaded into the optimizer.
     """
+    score = 99.9
+
     trial_fail = exp_storage_memory_sql.trial(tunable_groups)
     trial_succ = exp_storage_memory_sql.trial(tunable_groups)
     trial_pend = exp_storage_memory_sql.trial(tunable_groups)
+
     trial_fail.update(Status.FAILED)
-    trial_succ.update(Status.SUCCEEDED, {'score': 99.9})
+    trial_succ.update(Status.SUCCEEDED, {'score': score})
+
     (pending,) = list(exp_storage_memory_sql.pending())
     assert pending.trial_id == trial_pend.trial_id
+
+    (configs, scores) = exp_storage_memory_sql.load('score')
+    assert len(configs) == 1
+    assert len(scores) == 1
+    assert scores[0] == score
+    assert tunable_groups.copy().assign(configs[0]).reset() == trial_succ.tunables
