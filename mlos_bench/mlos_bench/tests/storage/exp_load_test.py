26eff5a4cdbfed4ecd7a29a89b0013befdeb9fdc
Sergiy Matusevych <sergiym@microsoft.com>
2023-03-29 12:11:40 -0700

    iadd unit tests for retrieving pending trials

--- mlos_bench/mlos_bench/tests/storage/exp_load_test.py
+++ mlos_bench/mlos_bench/tests/storage/exp_load_test.py
@@ -8,6 +8,7 @@ Unit tests for the storage subsystem.
 
 from mlos_bench.storage import Storage
 from mlos_bench.tunables import TunableGroups
+from mlos_bench.environment import Status
 
 # pylint: disable=redefined-outer-name
 
@@ -37,3 +38,52 @@ def test_exp_trial_pending(exp_storage_memory_sql: Storage.Experiment,
     trial = exp_storage_memory_sql.trial(tunable_groups)
     (pending,) = list(exp_storage_memory_sql.pending())
     assert pending.trial_id == trial.trial_id
+
+
+def test_exp_trial_pending_2(exp_storage_memory_sql: Storage.Experiment,
+                             tunable_groups: TunableGroups):
+    """
+    Start TWO trials and check that both are pending.
+    """
+    trial1 = exp_storage_memory_sql.trial(tunable_groups)
+    trial2 = exp_storage_memory_sql.trial(tunable_groups)
+    pending = list(exp_storage_memory_sql.pending())
+    assert len(pending) == 2
+    assert {pending[0].trial_id, pending[1].trial_id} == {trial1.trial_id, trial2.trial_id}
+
+
+def test_exp_trial_pending_fail(exp_storage_memory_sql: Storage.Experiment,
+                                tunable_groups: TunableGroups):
+    """
+    Start a trial, fail it, and and check that it is NOT pending.
+    """
+    trial = exp_storage_memory_sql.trial(tunable_groups)
+    trial.update(Status.FAILED)
+    trials = list(exp_storage_memory_sql.pending())
+    assert not trials
+
+
+def test_exp_trial_success(exp_storage_memory_sql: Storage.Experiment,
+                           tunable_groups: TunableGroups):
+    """
+    Start a trial, finish it successfully, and and check that it is NOT pending.
+    """
+    trial = exp_storage_memory_sql.trial(tunable_groups)
+    trial.update(Status.SUCCEEDED, {'score': 99.9})
+    trials = list(exp_storage_memory_sql.pending())
+    assert not trials
+
+
+def test_exp_trial_pending_3(exp_storage_memory_sql: Storage.Experiment,
+                             tunable_groups: TunableGroups):
+    """
+    Start THREE trials, let one succeed, another one fail,
+    and check that one is still pending.
+    """
+    trial_fail = exp_storage_memory_sql.trial(tunable_groups)
+    trial_succ = exp_storage_memory_sql.trial(tunable_groups)
+    trial_pend = exp_storage_memory_sql.trial(tunable_groups)
+    trial_fail.update(Status.FAILED)
+    trial_succ.update(Status.SUCCEEDED, {'score': 99.9})
+    (pending,) = list(exp_storage_memory_sql.pending())
+    assert pending.trial_id == trial_pend.trial_id
