26e6f35b7af9bfea78d58f77dc34d407fd08b284
Brian Kroth <bpkroth@microsoft.com>
2023-04-10 14:47:25 +0000

    improvements

--- scripts/dmypy-wrapper.sh
+++ scripts/dmypy-wrapper.sh
@@ -8,18 +8,17 @@ set -eu
 scriptdir=$(dirname "$(readlink -f "$0")")
 cd "$scriptdir/.."
 
-# Make sure there's only a single dmypy running.
-if [ $(pgrep -fla bin/dmypy[^-] | wc -l) -gt 1 ]; then
-    pkill -f bin/dmypy[^-]
-fi
+DMYPY_STATUS_FILE='.dmypy.json'
+DMYPY_STATUS_ARGS="--status-file $DMYPY_STATUS_FILE"
+DMYPY_START_ARGS='--pretty --cache-fine-grained --install-types --non-interactive'
 
-dmypy status >/dev/null || dmypy start >/dev/null
+dmypy $DMYPY_STATUS_ARGS status >/dev/null || dmypy $DMYPY_STATUS_ARGS start -- $DMYPY_START_ARGS
 
 # Restart the daemon if the config file has changed.
-if [ setup.cfg -nt /proc/$(pgrep -f bin/dmypy[^-]) ]; then
-    dmypy restart >/dev/null
+if [ setup.cfg -nt /proc/$(cat $DMYPY_STATUS_FILE | jq -e -r .pid) ]; then
+    dmypy $DMYPY_STATUS_ARGS restart -- $DMYPY_START_ARGS
 fi
 
 # Check the files passed as arguments.
 # FIXME: would have to remove some args to use with vscode.
-dmypy check $*
+dmypy $DMYPY_STATUS_ARGS check $*
