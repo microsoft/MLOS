ee827c0aa9d92a7a3910ccb60cdadf2e20fb6cd3
Brian Kroth <bpkroth@microsoft.com>
2023-04-10 15:54:29 +0000

    more upstream ignores for type checking

--- mlos_core/setup.py
+++ mlos_core/setup.py
@@ -39,6 +39,9 @@ setup(
     name='mlos-core',
     version=_VERSION,
     packages=find_packages(),
+    package_data={
+        'mlos_core': ['py.typed'],
+    },
     install_requires=[
         'scikit-learn<1.2', # FIXME: temporarily work around some version mismatch issues (PR 850)
         'joblib>=1.1.1',    # CVE-2022-21797: scikit-learn dependency, addressed in 1.2.0dev0, which isn't currently released
