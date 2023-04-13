6bd0174e916a3d30101e0656028a2f68e85a27d8
Brian Kroth <bpkroth@microsoft.com>
2023-04-13 20:35:00 +0000

    more typing fixups

--- mlos_bench/mlos_bench/run_opt.py
+++ mlos_bench/mlos_bench/run_opt.py
@@ -9,16 +9,19 @@ OS Autotune main optimization loop.
 See `--help` output for details.
 """
 
+from typing import Tuple
+
 import logging
 
 from mlos_bench.launcher import Launcher
 from mlos_bench.optimizer import Optimizer
 from mlos_bench.environment import Status, Environment
+from mlos_bench.tunables.tunable_groups import TunableGroups
 
 _LOG = logging.getLogger(__name__)
 
 
-def _main():
+def _main() -> None:
 
     launcher = Launcher("mlos_bench run_opt")
 
@@ -40,7 +43,7 @@ def _main():
     _LOG.info("Final result: %s", result)
 
 
-def _optimize(env: Environment, opt: Optimizer, no_teardown: bool):
+def _optimize(env: Environment, opt: Optimizer, no_teardown: bool) -> Tuple[float, TunableGroups]:
     """
     Main optimization loop.
     """
@@ -57,7 +60,11 @@ def _optimize(env: Environment, opt: Optimizer, no_teardown: bool):
             continue
 
         (status, output) = env.run()  # Block and wait for the final result
-        value = output['score'] if status.is_succeeded else None
+        if status.is_succeeded:
+            assert output is not None
+            value = output['score']
+        else:
+            value = None
 
         _LOG.info("Result: %s = %s :: %s", tunables, status, value)
         opt.register(tunables, status, value)
