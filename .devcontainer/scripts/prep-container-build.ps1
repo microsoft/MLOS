52d0870cd50c1e22fd4a5f4bdfef6a1bb597e26c
Brian Kroth <bpkroth@microsoft.com>
2023-04-14 18:21:58 +0000

    make it work for powershell too

--- .devcontainer/scripts/prep-container-build.ps1
+++ .devcontainer/scripts/prep-container-build.ps1
@@ -14,6 +14,10 @@ if (!(Test-Path .env)) {
     Write-Host "Creating empty .env file for devcontainer."
     New-Item -Type File .env
 }
+if (!(Get-Content .\.env | Select-String '^NGINX_PORT=[0-9]+$')) {
+    $NGINX_PORT = (Get-Random) % 30000 + 80
+    echo "NGINX_PORT=$NGINX_PORT" >> .env
+}
 
 # Prep some files to use as context for the devcontainer to build from.
 if (Test-Path .devcontainer/tmp) {
