20349f1a5368efa7b85f64624b545eae7a54dd37
Brian Kroth <bpkroth@microsoft.com>
2023-04-17 22:13:41 +0000

    unnecessary

--- mlos_bench/mlos_bench/service/local/__init__.py
+++ mlos_bench/mlos_bench/service/local/__init__.py
@@ -6,11 +6,9 @@
 Local scheduler side Services for mlos_bench.
 """
 
-from mlos_bench.service.types.local_exec_type import SupportsLocalExec
 from mlos_bench.service.local.local_exec import LocalExecService
 
 
 __all__ = [
-    'SupportsLocalExec',
     'LocalExecService',
 ]
