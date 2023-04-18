64c43d3acf8a92e5555a5c5fedb4f3ec62752af0
Sergiy Matusevych <sergiym@microsoft.com>
2023-04-18 15:23:09 -0700

    add unit tests for config storage and retrieval

--- mlos_bench/mlos_bench/tests/storage/trial_config_test.py
+++ mlos_bench/mlos_bench/tests/storage/trial_config_test.py
@@ -26,3 +26,37 @@ def test_exp_trial_pending(exp_storage_memory_sql: Storage.Experiment,
         "experimentId": "Test-001",
         "trialId": 1,
     }
+
+
+def test_exp_trial_configs(exp_storage_memory_sql: Storage.Experiment,
+                           tunable_groups: TunableGroups) -> None:
+    """
+    Start multiple trials with two different configs and check that
+    we store only two config objects in the DB.
+    """
+    config1 = tunable_groups.copy().assign({'rootfs': 'ext4'})
+    trials1 = [
+        exp_storage_memory_sql.new_trial(config1),
+        exp_storage_memory_sql.new_trial(config1),
+        exp_storage_memory_sql.new_trial(config1),
+    ]
+    assert trials1[0].config_id == trials1[1].config_id
+    assert trials1[0].config_id == trials1[2].config_id
+
+    config2 = tunable_groups.copy().assign({'rootfs': 'ext4'})
+    trials2 = [
+        exp_storage_memory_sql.new_trial(config2),
+        exp_storage_memory_sql.new_trial(config2),
+        exp_storage_memory_sql.new_trial(config2),
+    ]
+    assert trials2[0].config_id == trials2[1].config_id
+    assert trials2[0].config_id == trials2[2].config_id
+
+    assert trials1[0].config_id != trials2[0].config_id
+
+    pending_ids = [
+        pending.config_id for pending in exp_storage_memory_sql.pending_trials()
+    ]
+    assert len(pending_ids) == 6
+    assert len(set(pending_ids)) == 2
+    assert set(pending_ids) == {trials1[0].config_id, trials2[0].config_id}
