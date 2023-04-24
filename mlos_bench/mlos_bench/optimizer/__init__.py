1aeb71da13475dda8150081af623a82d3f3bc945
Sergiy Matusevych <sergiym@microsoft.com>
2023-04-24 13:57:11 -0700

    implement null optimizer to run one iteration with a given config

--- mlos_bench/mlos_bench/optimizer/__init__.py
+++ mlos_bench/mlos_bench/optimizer/__init__.py
@@ -8,10 +8,12 @@ Interfaces and wrapper classes for optimizers to be used in Autotune.
 
 from mlos_bench.optimizer.base_optimizer import Optimizer
 from mlos_bench.optimizer.mock_optimizer import MockOptimizer
+from mlos_bench.optimizer.null_optimizer import NullOptimizer
 from mlos_bench.optimizer.mlos_core_optimizer import MlosCoreOptimizer
 
 __all__ = [
     'Optimizer',
     'MockOptimizer',
+    'NullOptimizer',
     'MlosCoreOptimizer',
 ]
