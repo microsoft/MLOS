1e266178f8f7b1fc07a1c562426e26bc0ce91f7a
Brian Kroth <bpkroth@microsoft.com>
2023-04-26 21:29:50 +0000

    fixups

--- mlos_bench/mlos_bench/tests/config/services/test_load_service_config_examples.py
+++ mlos_bench/mlos_bench/tests/config/services/test_load_service_config_examples.py
@@ -43,13 +43,10 @@ assert configs
 def test_load_service_config_examples(config_loader_service: ConfigPersistenceService, config_path: str) -> None:
     """Tests loading a config example."""
     config = config_loader_service.load_config(config_path)
-    assert isinstance(config, dict)
-    cls = get_class_from_name(config["class"])
-    assert issubclass(cls, Service)
     # Make an instance of the class based on the config.
     service_inst = config_loader_service.build_service(
         config=config,
         parent=config_loader_service,
     )
     assert service_inst is not None
-    assert isinstance(service_inst, cls)
+    assert isinstance(service_inst, Service)
