75aa43ad549373d7f35a2a1543dac5ae924f8759
Brian Kroth <bpkroth@microsoft.com>
2023-04-26 20:28:03 +0000

    cleanup storage loading tests

--- mlos_bench/mlos_bench/tests/config/__init__.py
+++ mlos_bench/mlos_bench/tests/config/__init__.py
@@ -2,9 +2,17 @@
 Helper functions for config example loading tests.
 """
 
+from typing import Any, List, Optional
+
+import importlib
+import logging
 import os
 
-from typing import List
+from mlos_bench.services.config_persistence import ConfigPersistenceService
+from mlos_bench.tunables.tunable_groups import TunableGroups
+
+_LOG = logging.getLogger(__name__)
+_LOG.setLevel(logging.DEBUG)
 
 
 def locate_config_examples(config_examples_dir: str) -> List[str]:
@@ -26,3 +34,26 @@ def locate_config_examples(config_examples_dir: str) -> List[str]:
             if file.endswith(".json") or file.endswith(".jsonc"):
                 config_examples.append(os.path.join(root, file))
     return config_examples
+
+
+def load_config_example(config_path: str, config_overrides: Optional[dict] = None) -> Any:
+    """Tests loading a config example."""
+    config_loader_service = ConfigPersistenceService()
+    _LOG.info("Loading config %s", config_path)
+    config = config_loader_service.load_config(config_path)
+    assert isinstance(config, dict)
+    if config_overrides is not None:
+        config.update(config_overrides)
+
+    cls_fqn: str = config["class"]
+    _LOG.info("Loading %s", cls_fqn)
+    module = importlib.import_module(str.join(".", (cls_fqn.split(".")[0:-1])))
+    cls = getattr(module, cls_fqn.split(".")[-1])
+    assert isinstance(cls, type)
+
+    return config_loader_service.build_generic(
+        base_cls=cls,
+        tunables=TunableGroups(),
+        service=config_loader_service,
+        config=config,
+    )
--- mlos_bench/mlos_bench/tests/config/storage/test_load_storage_config_examples.py
+++ mlos_bench/mlos_bench/tests/config/storage/test_load_storage_config_examples.py
@@ -14,7 +14,7 @@ import os
 
 import pytest
 
-from mlos_bench.tests.config import locate_config_examples
+from mlos_bench.tests.config import locate_config_examples, load_config_example
 from mlos_bench.services.config_persistence import ConfigPersistenceService
 from mlos_bench.storage.base_storage import Storage
 from mlos_bench.tunables.tunable_groups import TunableGroups
@@ -40,22 +40,12 @@ assert configs
 @pytest.mark.parametrize("config_path", configs)
 def test_load_storage_config_examples(config_path: str) -> None:
     """Tests loading a config example."""
-    config_loader_service = ConfigPersistenceService()
-    _LOG.info("Loading config %s", config_path)
-    config = config_loader_service.load_config(config_path)
-    assert isinstance(config, dict)
-    #config["config"]["lazy_connect"] = True
-    config["config"]["lazy_schema_create"] = True
-    cls_fqn: str = config["class"]
-    _LOG.info("Loading %s", cls_fqn)
-    module = importlib.import_module(str.join(".", (cls_fqn.split(".")[0:-1])))
-    cls = getattr(module, cls_fqn.split(".")[-1])
-    assert isinstance(cls, type)
-    storage_inst = config_loader_service.build_generic(
-        base_cls=cls,
-        tunables=TunableGroups(),
-        service=config_loader_service,
-        config=config,
-    )
+    # Skip schema loading that would require a database connection for this test.
+    storage_inst = load_config_example(config_path, config_overrides={
+        "config": {
+            "lazy_schema_create": True,
+        }
+    })
+
     assert storage_inst is not None
     assert isinstance(storage_inst, Storage)
