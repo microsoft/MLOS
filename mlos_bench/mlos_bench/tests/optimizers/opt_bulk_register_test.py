c0d1b2df58724585a6643cd75a4218ac8e9b0886
Brian Kroth <bpkroth@microsoft.com>
2023-05-01 23:48:29 +0000

    fixups

--- mlos_bench/mlos_bench/tests/optimizers/opt_bulk_register_test.py
+++ mlos_bench/mlos_bench/tests/optimizers/opt_bulk_register_test.py
@@ -26,25 +26,25 @@ def mock_configs() -> List[dict]:
     return [
         {
             'vmSize': 'Standard_B4ms',
-            'rootfs': 'xfs',
+            'idle': 'halt',
             'kernel_sched_migration_cost_ns': 50000,
             'kernel_sched_latency_ns': 1000000,
         },
         {
             'vmSize': 'Standard_B4ms',
-            'rootfs': 'xfs',
+            'idle': 'halt',
             'kernel_sched_migration_cost_ns': 40000,
             'kernel_sched_latency_ns': 2000000,
         },
         {
             'vmSize': 'Standard_B4ms',
-            'rootfs': 'ext4',
+            'idle': 'mwait',
             'kernel_sched_migration_cost_ns': 100000,
             'kernel_sched_latency_ns': 3000000,
         },
         {
             'vmSize': 'Standard_B2s',
-            'rootfs': 'xfs',
+            'idle': 'mwait',
             'kernel_sched_migration_cost_ns': 200000,
             'kernel_sched_latency_ns': 4000000,
         }
@@ -78,7 +78,7 @@ def _test_opt_update_min(opt: Optimizer, configs: List[dict],
     assert tunables is not None
     assert tunables.get_param_values() == {
         "vmSize": "Standard_B4ms",
-        "rootfs": "ext4",
+        "idle": "mwait",
         "kernel_sched_migration_cost_ns": 100000,
         'kernel_sched_latency_ns': 3000000,
     }
@@ -95,7 +95,7 @@ def _test_opt_update_max(opt: Optimizer, configs: List[dict],
     assert tunables is not None
     assert tunables.get_param_values() == {
         "vmSize": "Standard_B2s",
-        "rootfs": "xfs",
+        "idle": "mwait",
         "kernel_sched_migration_cost_ns": 200000,
         'kernel_sched_latency_ns': 4000000,
     }
