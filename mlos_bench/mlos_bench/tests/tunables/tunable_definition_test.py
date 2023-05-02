81c6ef8d0379e494f31b95c9233b4cb5910c77b6
Brian Kroth <bpkroth@microsoft.com>
2023-05-02 15:27:45 +0000

    add a test for duplicate values being disallowed

---
+++ mlos_bench/mlos_bench/tests/tunables/tunable_definition_test.py
@@ -0,0 +1,22 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+"""
+Unit tests for assigning values to the individual parameters within tunable groups.
+"""
+
+import pytest
+
+from mlos_bench.tunables.tunable import Tunable
+
+def test_categorical_tunable_disallow_repeats() -> None:
+    """
+    Disallow duplicate values in categorical tunables.
+    """
+    with pytest.raises(ValueError):
+        Tunable(name='test', config={
+            "type": "categorical",
+            "values": ["foo", "bar", "foo"],
+            "default": "foo",
+        })
