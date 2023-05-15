fd0472bd401728193d2f8865cd6ce5a251046042
Brian Kroth <bpkroth@microsoft.com>
2023-05-15 16:26:50 +0000

    adding tests for cli config example loading

--- mlos_bench/mlos_bench/tests/config/cli/test_load_cli_config_examples.py
+++ mlos_bench/mlos_bench/tests/config/cli/test_load_cli_config_examples.py
@@ -16,7 +16,7 @@ from mlos_bench.tests.config import locate_config_examples
 
 from mlos_bench.config.schemas import ConfigSchema
 from mlos_bench.services.config_persistence import ConfigPersistenceService
-from mlos_bench.util import get_class_from_name, path_join
+from mlos_bench.util import path_join
 
 
 _LOG = logging.getLogger(__name__)
@@ -41,21 +41,39 @@ def test_load_cli_config_examples(config_loader_service: ConfigPersistenceServic
     """Tests loading a config example."""
     config = config_loader_service.load_config(config_path, ConfigSchema.CLI)
     assert isinstance(config, dict)
-    # TODO: process "config_path" first.
 
-    # TODO: for each arg that references another file, see if we can at least load that too.
-    args_to_skip = ["log_file", "log_level", "experimentId", "trialId", "teardown"]
+    if config_paths := config.get("config_path"):
+        assert isinstance(config_paths, list)
+        config_paths.reverse()
+        for path in config_paths:
+            config_loader_service._config_path.insert(0, path)   # pylint: disable=protected-access
+
+    # Foreach arg that references another file, see if we can at least load that too.
+    args_to_skip = {
+        "config_path",  # handled above
+        "globals",      # we don't commit globals to the repo generally, so skip testing them
+        "log_file",
+        "log_level",
+        "experimentId",
+        "trialId",
+        "teardown",
+    }
     for arg in config:
         if arg in args_to_skip:
             continue
-        if arg == "globals":
-            # TODO: attempt to load the globals file, but allow it to not exist
-            raise NotImplementedError("TODO")
-        elif arg == "environment":
-            raise NotImplementedError("TODO")
+
+        if arg == "environment":
+            sub_config = config_loader_service.load_config(config[arg], schema_type=None)   # TODO: ConfigSchema.Environment
+            assert isinstance(sub_config, dict)
         elif arg == "optimizer":
-            raise NotImplementedError("TODO")
+            sub_config = config_loader_service.load_config(config[arg], ConfigSchema.OPTIMIZER)
+            assert isinstance(sub_config, dict)
         elif arg == "storage":
-            raise NotImplementedError("TODO")
+            sub_config = config_loader_service.load_config(config[arg], ConfigSchema.STORAGE)
+            assert isinstance(sub_config, dict)
+        elif arg == "tunable_values":
+            for path in config[arg]:
+                sub_config = config_loader_service.load_config(path, ConfigSchema.TUNABLE_VALUES)
+                assert isinstance(sub_config, dict)
         else:
             raise NotImplementedError(f"Unhandled arg {arg} in config {config_path}")
