72098ca7872fea2824328d1aa5c3d6245f7f4f6b
Brian Kroth <bpkroth@microsoft.com>
2023-05-16 20:56:26 +0000

    fixup

--- mlos_bench/mlos_bench/tests/config/schemas/globals/test_globals_schemas.py
+++ mlos_bench/mlos_bench/tests/config/schemas/globals/test_globals_schemas.py
@@ -27,16 +27,8 @@ TEST_CASES = get_schema_test_cases(path.join(path.dirname(__file__), "test-cases
 # Now we actually perform all of those validation tests.
 
 @pytest.mark.parametrize("test_case_name", sorted(TEST_CASES.by_path))
-def test_cli_configs_against_schema(test_case_name: str) -> None:
+def test_globals_configs_against_schema(test_case_name: str) -> None:
     """
     Checks that the CLI config validates against the schema.
     """
     check_test_case_against_schema(TEST_CASES.by_path[test_case_name], ConfigSchema.GLOBALS)
-
-
-def test_optimizer_configs_with_extra_param() -> None:
-    """
-    Checks that the cli config fails to validate if extra params are present in certain places.
-    """
-    test_case = next(iter(TEST_CASES.by_type["good"].values()))
-    check_test_case_config_with_extra_param(test_case, ConfigSchema.GLOBALS)
