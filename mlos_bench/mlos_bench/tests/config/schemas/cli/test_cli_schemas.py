e7fb39ed8d0f8283fd84a5da034624f7b8ea0ee9
Brian Kroth <bpkroth@microsoft.com>
2023-05-18 21:46:25 +0000

    other schema test fixups

--- mlos_bench/mlos_bench/tests/config/schemas/cli/test_cli_schemas.py
+++ mlos_bench/mlos_bench/tests/config/schemas/cli/test_cli_schemas.py
@@ -34,9 +34,9 @@ def test_cli_configs_against_schema(test_case_name: str) -> None:
     check_test_case_against_schema(TEST_CASES.by_path[test_case_name], ConfigSchema.CLI)
 
 
-def test_optimizer_configs_with_extra_param() -> None:
+@pytest.mark.parametrize("test_case_name", sorted(TEST_CASES.by_type["good"]))
+def test_cli_configs_with_extra_param(test_case_name: str) -> None:
     """
     Checks that the cli config fails to validate if extra params are present in certain places.
     """
-    test_case = next(iter(TEST_CASES.by_type["good"].values()))
-    check_test_case_config_with_extra_param(test_case, ConfigSchema.CLI)
+    check_test_case_config_with_extra_param(TEST_CASES.by_type["good"][test_case_name], ConfigSchema.CLI)
