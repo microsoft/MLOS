54ca3d5f8f60c1930801d9e9ae5e42587ae5c0a9
Brian Kroth <bpkroth@microsoft.com>
2023-05-19 20:02:33 +0000

    fixup

--- mlos_bench/mlos_bench/tests/config/schemas/services/test_services_schemas.py
+++ mlos_bench/mlos_bench/tests/config/schemas/services/test_services_schemas.py
@@ -58,7 +58,7 @@ def test_service_configs_against_schema(test_case_name: str) -> None:
     """
     Checks that the service config validates against the schema.
     """
-    check_test_case_against_schema(TEST_CASES.by_path[test_case_name], ConfigSchema.service)
+    check_test_case_against_schema(TEST_CASES.by_path[test_case_name], ConfigSchema.SERVICE)
 
 
 @pytest.mark.parametrize("test_case_name", sorted(TEST_CASES.by_type["good"]))
@@ -66,4 +66,4 @@ def test_service_configs_with_extra_param(test_case_name: str) -> None:
     """
     Checks that the service config fails to validate if extra params are present in certain places.
     """
-    check_test_case_config_with_extra_param(TEST_CASES.by_type["good"][test_case_name], ConfigSchema.service)
+    check_test_case_config_with_extra_param(TEST_CASES.by_type["good"][test_case_name], ConfigSchema.SERVICE)
