aca2ab15ecb56ff92e6d70b9b533e033e6c90700
Brian Kroth <bpkroth@microsoft.com>
2023-05-22 18:19:35 +0000

    provide some init completions

--- mlos_bench/mlos_bench/tests/services/__init__.py
+++ mlos_bench/mlos_bench/tests/services/__init__.py
@@ -6,3 +6,13 @@
 Tests for mlos_bench.services.
 Used to make mypy happy about multiple conftest.py modules.
 """
+
+from .local import MockLocalExecService
+from .remote import MockFileShareService, MockRemoteExecService, MockVMService
+
+__all__ = [
+    'MockLocalExecService',
+    'MockFileShareService',
+    'MockRemoteExecService',
+    'MockVMService',
+]
