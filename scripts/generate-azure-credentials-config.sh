a2114f3b1493f5262817e5b90f882d951cd74834
Brian Kroth <bpkroth@microsoft.com>
2023-06-23 17:10:14 +0000

    generalize which rg and account to fetch tokens for

--- scripts/generate-azure-credentials-config.sh
+++ scripts/generate-azure-credentials-config.sh
@@ -4,9 +4,15 @@
 ## Licensed under the MIT License.
 ##
 
+set -eu
+set -x
+
+AZURE_DEFAULTS_GROUP=${AZURE_DEFAULTS_GROUP:-$(az config get defaults.group --query value -o tsv)}
+AZURE_STORAGE_ACCOUNT_NAME=${AZURE_STORAGE_ACCOUNT_NAME:-$(az config get storage.account --query value -o tsv)}
+
 az account get-access-token |
     jq ".storageAccountKey = `
         az storage account keys list \
-            --resource-group os-autotune \
-            --account-name osatsharedstorage \
+            --resource-group $AZURE_DEFAULTS_GROUP \
+            --account-name $AZURE_STORAGE_ACCOUNT_NAME \
             --query '[0].value'`"
