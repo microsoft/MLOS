b251702af1c1d54854d98d593c444ce0a6b33f97
Brian Kroth <bpkroth@microsoft.com>
2023-07-06 20:26:04 +0000

    Schema resolution fixups for new version of jsonschema with referencing lib.

--- mlos_bench/setup.py
+++ mlos_bench/setup.py
@@ -76,7 +76,7 @@ setup(
         'mlos-core==' + _VERSION,
         'requests',
         'json5',
-        'jsonschema',
+        'jsonschema>=4.18.0', 'referencing>=0.29.1',
         'importlib_resources;python_version<"3.10"',
     ] + extra_requires['storage-sql-sqlite'],   # NOTE: For now sqlite is a fallback storage backend, so we always install it.
     extras_require=extra_requires,
