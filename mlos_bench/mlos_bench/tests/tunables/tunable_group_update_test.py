55f8abbc81121f99728b3137bef0fe1e567d940c
Sergiy Matusevych <sergiym@microsoft.com>
2023-07-18 15:45:57 -0700

    more unit tests to verify the tunable_groups assignment behavior

--- mlos_bench/mlos_bench/tests/tunables/tunable_group_update_test.py
+++ mlos_bench/mlos_bench/tests/tunables/tunable_group_update_test.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License.
 #
 """
-Tests for checking the indexing rules for tunable groups.
+Tests for checking the is_updated flag for tunable groups.
 """
 
 from mlos_bench.tunables.tunable_groups import TunableGroups
@@ -34,3 +34,28 @@ def test_tunable_group_update_twice(tunable_groups: TunableGroups) -> None:
 
     tunable_groups.assign(_TUNABLE_VALUES)
     assert not tunable_groups.is_updated()
+
+
+def test_tunable_group_update_kernel(tunable_groups: TunableGroups) -> None:
+    """
+    Test that the is_updated flag is set only for the affected covariant group.
+    """
+    tunable_groups.assign(_TUNABLE_VALUES)
+    assert tunable_groups.is_updated()
+    assert tunable_groups.is_updated(["kernel"])
+    assert not tunable_groups.is_updated(["boot", "provision"])
+
+
+def test_tunable_group_update_boot(tunable_groups: TunableGroups) -> None:
+    """
+    Test that the is_updated flag is set only for the affected covariant group.
+    """
+    tunable_groups.assign(_TUNABLE_VALUES)
+    assert tunable_groups.is_updated()
+    assert not tunable_groups.is_updated(["boot"])
+
+    tunable_groups.reset()
+    tunable_groups["idle"] = "mwait"
+    assert tunable_groups.is_updated()
+    assert tunable_groups.is_updated(["boot"])
+    assert not tunable_groups.is_updated(["kernel"])
