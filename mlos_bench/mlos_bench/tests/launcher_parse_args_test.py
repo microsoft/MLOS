33529a0c2e795b42e6825fe942353f645ca78e60
Brian Kroth <bpkroth@microsoft.com>
2023-08-21 22:01:08 +0000

    tests

--- mlos_bench/mlos_bench/tests/launcher_parse_args_test.py
+++ mlos_bench/mlos_bench/tests/launcher_parse_args_test.py
@@ -34,18 +34,39 @@ def config_paths(root_path: str) -> List[str]:
     List[str]
     """
     return [
-        path_join(root_path, "mlos_bench/mlos_bench/config"),
-        path_join(root_path, "mlos_bench/mlos_bench/tests/config"),
+        path_join(root_path, 'mlos_bench/mlos_bench/config'),
+        path_join(root_path, 'mlos_bench/mlos_bench/tests/config'),
     ]
 
 
-def test_launcher_args_parse_list_append(config_paths: List[str]) -> None:
+@pytest.fixture
+def minimal_launcher_args(config_paths: List[str]) -> List[str]:
+    """
+    Returns a list of minimal args necessary for the Launcher.
+    """
+    return [
+        '--config-paths', *config_paths,    # provides two config paths as space separate args
+        '--environment', 'environments/mock/mock_env.jsonc',
+    ]
+
+
+def test_launcher_args_parse_globals(minimal_launcher_args: List[str]) -> None:
     """
     Test argument parsing.
     """
     launcher = Launcher(description="test", argv=[
-        "--config-paths", *config_paths,
-        "--globals", "globals/global_test_config.jsonc",
-        "--globals", "globals/global_test_extra_config.jsonc",
+        *minimal_launcher_args,
+        # Check that both --globals files are loaded when multiple args are provided.
+        '--globals', 'globals/global_test_config.jsonc',
+        '--globals', 'globals/global_test_extra_config.jsonc',
+        # Check that --some-unknown-key values are loaded into the global config.
+        '--test_global_value_2', 'from-args',
     ])
-    assert 'test_global_value' in launcher.global_config
+    # Check that the first --globals file is loaded and $var expansion is handled.
+    assert launcher.global_config['experiment_id'] == 'MockExperiment'
+    assert launcher.global_config['testVmName'] == 'MockExperiment-vm'
+    assert launcher.global_config['testVnetName'] == 'MockExperiment-vnet'
+    # Check that the second --globals file is loaded.
+    assert launcher.global_config['test_global_value'] == 'from-file'
+    # Check overriding values in a file from the command line.
+    assert launcher.global_config['test_global_value_2'] == 'from-args'
