e3150ce494f3de2693feef17626ceed23dd34529
Brian Kroth <bpkroth@microsoft.com>
2023-08-23 22:25:04 +0000

    more tests

--- mlos_bench/mlos_bench/tests/launcher_parse_args_test.py
+++ mlos_bench/mlos_bench/tests/launcher_parse_args_test.py
@@ -40,20 +40,14 @@ def config_paths() -> List[str]:
     ]
 
 
-@pytest.fixture
-def env_conf_path() -> str:
-    """
-    Returns the path to a test environment config file.
-    This is part of the minimal required args by the Launcher.
-    """
-    return 'environments/mock/mock_env.jsonc'
-
-
-def test_launcher_args_parse_globals(config_paths: List[str], env_conf_path: str) -> None:
+def test_launcher_args_parse_1(config_paths: List[str]) -> None:
     """
     Test that using multiple --globals arguments works and that multiple space
     separated options to --config-paths works.
+    Check $var expansion and Environment loading.
     """
+    # This is part of the minimal required args by the Launcher.
+    env_conf_path = 'environments/mock/mock_env.jsonc'
     cli_args = '--config-paths ' + ' '.join(config_paths) + \
         f' --environment {env_conf_path}' + \
         ' --globals globals/global_test_config.jsonc' + \
@@ -70,18 +64,25 @@ def test_launcher_args_parse_globals(config_paths: List[str], env_conf_path: str
     # Check overriding values in a file from the command line.
     assert launcher.global_config['test_global_value_2'] == 'from-args'
     assert launcher.teardown
+    # Check that the environment that got loaded looks to be of the right type.
+    env_config = launcher.config_loader.load_config(env_conf_path, ConfigSchema.ENVIRONMENT)
+    assert check_class_name(launcher.environment, env_config['class'])
 
 
-def test_launcher_args_parse_multiple_config_paths(config_paths: List[str], env_conf_path: str) -> None:
+def test_launcher_args_parse_2(config_paths: List[str]) -> None:
     """
-    Test multiple --config-path instances.
+    Test multiple --config-path instances, --config file vs --arg, --var=val
+    overrides, $var templates, option args, etc.
     """
+    config_file = 'cli/test-cli-config.jsonc'
     cli_args = ' '.join([f"--config-path {config_path}" for config_path in config_paths]) + \
-        f' --environment {env_conf_path}' + \
+        f' --config {config_file}' + \
         ' --globals globals/global_test_config.jsonc' + \
         ' --experiment_id MockeryExperiment' + \
-        ' --no-teardown'
-    launcher = Launcher(description="test", argv=cli_args.split())
+        ' --no-teardown' + \
+        ' --random-init' + \
+        ' --random-seed 1234'
+    launcher = Launcher(description=__name__, argv=cli_args.split())
     # Check that the --globals file is loaded and $var expansion is handled
     # using the value provided on the CLI.
     assert launcher.global_config['experiment_id'] == 'MockeryExperiment'
