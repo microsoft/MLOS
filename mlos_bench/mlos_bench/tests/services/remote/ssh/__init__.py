6c94be4b9f8b9c3ac6d6ecce35b620ab68db5bd6
Brian Kroth <bpkroth@microsoft.com>
2023-09-14 19:12:53 +0000

    todo comments

--- mlos_bench/mlos_bench/tests/services/remote/ssh/__init__.py
+++ mlos_bench/mlos_bench/tests/services/remote/ssh/__init__.py
@@ -10,6 +10,7 @@ from dataclasses import dataclass
 
 
 # The SSH test server port.
+# TODO: Make this random per test run to prevent overlaps between sessions.
 # See Also: docker-compose.yml
 SSH_TEST_SERVER_PORT = 2254
 
