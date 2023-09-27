d3101695dca298190f1566df9da880cc1894aad0
Brian Kroth <bpkroth@microsoft.com>
2023-09-27 23:08:31 +0000

    recursive file copy tests

--- mlos_bench/mlos_bench/tests/services/remote/ssh/test_ssh_fileshare.py
+++ mlos_bench/mlos_bench/tests/services/remote/ssh/test_ssh_fileshare.py
@@ -6,6 +6,13 @@
 Tests for mlos_bench.services.remote.ssh.ssh_services
 """
 
+from logging import warning
+from os.path import basename
+from pathlib import Path
+from typing import Dict, List
+
+import filecmp
+import os
 import tempfile
 
 import pytest
@@ -15,6 +22,7 @@ from asyncssh import SFTPError
 from mlos_bench.environments.status import Status
 from mlos_bench.services.remote.ssh.ssh_host_service import SshHostService
 from mlos_bench.services.remote.ssh.ssh_fileshare import SshFileShareService
+from mlos_bench.util import path_join
 
 from mlos_bench.tests import requires_docker
 from mlos_bench.tests.services.remote.ssh import SshTestServerInfo
@@ -55,12 +63,88 @@ def test_ssh_fileshare_single_file(ssh_test_server: SshTestServerInfo,
             assert read_lines == lines
 
 
+def are_dir_trees_equal(dir1: str, dir2: str) -> bool:
+    """
+    Compare two directories recursively. Files in each directory are
+    assumed to be equal if their names and contents are equal.
+
+    @param dir1: First directory path
+    @param dir2: Second directory path
+
+    @return: True if the directory trees are the same and
+        there were no errors while accessing the directories or files,
+        False otherwise.
+    """
+    # See Also: https://stackoverflow.com/a/6681395
+    dirs_cmp = filecmp.dircmp(dir1, dir2)
+    if len(dirs_cmp.left_only) > 0 or len(dirs_cmp.right_only) > 0 or len(dirs_cmp.funny_files) > 0:
+        warning(f"Found differences in dir trees {dir1}, {dir2}:\n{dirs_cmp.diff_files}\n{dirs_cmp.funny_files}")
+        return False
+    (_, mismatch, errors) = filecmp.cmpfiles(dir1, dir2, dirs_cmp.common_files, shallow=False)
+    if len(mismatch) > 0 or len(errors) > 0:
+        warning(f"Found differences in files:\n{mismatch}\n{errors}")
+        return False
+    for common_dir in dirs_cmp.common_dirs:
+        new_dir1 = os.path.join(dir1, common_dir)
+        new_dir2 = os.path.join(dir2, common_dir)
+        if not are_dir_trees_equal(new_dir1, new_dir2):
+            return False
+    return True
+
+
 @pytest.mark.xdist_group("ssh_test_server")
 @requires_docker
 def test_ssh_fileshare_recursive(ssh_test_server: SshTestServerInfo,
                                  ssh_fileshare_service: SshFileShareService) -> None:
     """Test the SshFileShareService recursive download/upload."""
-    raise NotImplementedError("TODO")
+    config = ssh_test_server.to_ssh_service_config()
+
+    remote_file_path = "/tmp/test_ssh_fileshare_recursive_dir"
+    files_lines: Dict[str, List[str]] = {
+        "file-a.txt": [
+            "a",
+            "1",
+        ],
+        "file-b.txt": [
+            "b",
+            "2",
+        ],
+        "subdir/foo.txt": [
+            "foo",
+            "bar",
+        ],
+    }
+    files_lines = {path: [line + "\n" for line in lines] for (path, lines) in files_lines.items()}
+
+    with tempfile.TemporaryDirectory() as tempdir1, tempfile.TemporaryDirectory() as tempdir2:
+        # Setup the directory structure.
+        for (file_path, lines) in files_lines.items():
+            path = Path(tempdir1, file_path)
+            path.parent.mkdir(parents=True, exist_ok=True)
+            with open(path, mode='w+t', encoding='utf-8') as temp_file:
+                temp_file.writelines(lines)
+                temp_file.flush()
+            assert os.path.getsize(path) > 0
+
+        # Copy that structure over to the remote server.
+        ssh_fileshare_service.upload(
+            params=config,
+            local_path=f"{tempdir1}",
+            remote_path=f"{remote_file_path}",
+            recursive=True,
+        )
+
+        # Copy the remote structure back to the local machine.
+        ssh_fileshare_service.download(
+            params=config,
+            remote_path=f"{remote_file_path}",
+            local_path=f"{tempdir2}",
+            recursive=True,
+        )
+
+        # Compare both.
+        # Note: remote dir name is appended to target.
+        assert are_dir_trees_equal(tempdir1, path_join(tempdir2, basename(remote_file_path)))
 
 
 @pytest.mark.xdist_group("ssh_test_server")
