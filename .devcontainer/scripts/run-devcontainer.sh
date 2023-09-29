370043c81bc637ebb055663c274b1e38dc95b354
Brian Kroth <bpkroth@microsoft.com>
2023-09-29 22:40:46 +0000

    allow nested run in container with cli args for testing

--- .devcontainer/scripts/run-devcontainer.sh
+++ .devcontainer/scripts/run-devcontainer.sh
@@ -19,18 +19,29 @@ cd "$repo_root"
 
 container_name="$repo_name.$(stat -c%i "$repo_root/")"
 
+# Be sure to use the host workspace folder if available.
+workspace_root=${LOCAL_WORKSPACE_FOLDER:-$repo_root}
+
+if [ -e /var/run/docker-host.sock ]; then
+    docker_gid=$(stat -c%g /var/run/docker-host.sock)
+else
+    docker_gid=$(stat -c%g /var/run/docker.sock)
+fi
+
+set -x
 mkdir -p "/tmp/$container_name/dc/shellhistory"
 docker run -it --rm \
     --name "$container_name" \
-    --user vscode \
+    --user vscode:$docker_gid \
     -v "$HOME/.azure":/dc/azure \
     -v "/tmp/$container_name/dc/shellhistory:/dc/shellhistory" \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$PWD":"/workspaces/$repo_name" \
+    -v "$workspace_root":"/workspaces/$repo_name" \
     --workdir "/workspaces/$repo_name" \
     --env CONTAINER_WORKSPACE_FOLDER="/workspaces/$repo_name" \
-    --env LOCAL_WORKSPACE_FOLDER="$repo_root" \
+    --env LOCAL_WORKSPACE_FOLDER="$workspace_root" \
     --env http_proxy="${http_proxy:-}" \
     --env https_proxy="${https_proxy:-}" \
     --env no_proxy="${no_proxy:-}" \
-    mlos-devcontainer
+    mlos-devcontainer \
+    $*
