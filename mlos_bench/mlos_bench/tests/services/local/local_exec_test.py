e1ff79ac17d06677f260f2e4195f4fc7cb07112a
Brian Kroth <bpkroth@users.noreply.github.com>
2023-10-10 10:20:05 -0500

    canonicalize the path for test comparison purposes

--- mlos_bench/mlos_bench/tests/services/local/local_exec_test.py
+++ mlos_bench/mlos_bench/tests/services/local/local_exec_test.py
@@ -222,4 +222,4 @@ def test_temp_dir_path_expansion() -> None:
         }
         local_exec_service = LocalExecService(config, global_config, parent=ConfigPersistenceService())
         # pylint: disable=protected-access
-        assert local_exec_service._temp_dir == path_join(temp_dir, "temp")
+        assert path_join(local_exec_service._temp_dir, abs_path=True) == path_join(temp_dir, "temp")
