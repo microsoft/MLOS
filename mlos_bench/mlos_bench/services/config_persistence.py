microsoft/MLOS

commit 6ccdb432a02e63c91cdf43f35ee40fe09f28f519

Misc bug fixes and improvements (#529)
- Adds support for expanding `temp_dir` in LocalExec service from the
globals.
- Also makes sure the CWD is on the config search path by default.
- Adds the ability to expand globals from Environment variables.
- Mostly useful to allow things like `$PWD` to be expanded to a fully
qualified path.
- Fix an errant debug message.

mlos_bench/mlos_bench/services/config_persistence.py
@@ -68,6 +68,11 @@ class ConfigPersistenceService(Service, SupportsConfigLoading):
         for path in self.config.get("config_path", []):
             if path not in self._config_path:
                 self._config_path.append(path_join(path, abs_path=True))
+        # Prepend the cwd if not already on the list.
+        cwd = os.path.abspath(os.getcwd())
+        if cwd not in self._config_path:
+            self._config_path.insert(0, cwd)
+        # Append the built-in config path if not already on the list.
         if self.BUILTIN_CONFIG_PATH not in self._config_path:
             self._config_path.append(self.BUILTIN_CONFIG_PATH)
 
@@ -114,12 +119,14 @@ class ConfigPersistenceService(Service, SupportsConfigLoading):
         """
         path_list = list(extra_paths or []) + self._config_path
         _LOG.debug("Resolve path: %s in: %s", file_path, path_list)
-        if not os.path.isabs(file_path):
-            for path in path_list:
-                full_path = path_join(path, file_path, abs_path=True)
-                if os.path.exists(full_path):
-                    _LOG.debug("Path resolved: %s", full_path)
-                    return full_path
+        if os.path.isabs(file_path):
+            _LOG.debug("Path is absolute: %s", file_path)
+            return file_path
+        for path in path_list:
+            full_path = path_join(path, file_path, abs_path=True)
+            if os.path.exists(full_path):
+                _LOG.debug("Path resolved: %s", full_path)
+                return full_path
         _LOG.debug("Path not resolved: %s", file_path)
         return file_path
 

mlos_bench/mlos_bench/tests/launcher_parse_args_test.py
@@ -6,6 +6,8 @@
 Unit tests to check the Launcher CLI arg parsing
 See Also: test_load_cli_config_examples.py
 """
+
+import os
 import sys
 from typing import List
 
@@ -35,6 +37,7 @@ def config_paths() -> List[str]:
     List[str]
     """
     return [
+        path_join(os.getcwd(), abs_path=True),
         str(files('mlos_bench.config')),
         str(files('mlos_bench.tests.config')),
     ]
@@ -46,6 +49,9 @@ def test_launcher_args_parse_1(config_paths: List[str]) -> None:
     separated options to --config-paths works.
     Check $var expansion and Environment loading.
     """
+    if sys.platform == 'win32':
+        os.environ['PWD'] = os.getcwd()
+
     # This is part of the minimal required args by the Launcher.
     env_conf_path = 'environments/mock/mock_env.jsonc'
     cli_args = '--config-paths ' + ' '.join(config_paths) + \
@@ -63,6 +69,8 @@ def test_launcher_args_parse_1(config_paths: List[str]) -> None:
     assert launcher.global_config['test_global_value'] == 'from-file'
     # Check overriding values in a file from the command line.
     assert launcher.global_config['test_global_value_2'] == 'from-args'
+    # Check that we can expand a $var in a config file that references an environment variable.
+    assert launcher.global_config["varWithEnvVarRef"] == path_join(os.getcwd(), "foo", abs_path=True)
     assert launcher.teardown
     # Check that the environment that got loaded looks to be of the right type.
     env_config = launcher.config_loader.load_config(env_conf_path, ConfigSchema.ENVIRONMENT)
@@ -78,6 +86,9 @@ def test_launcher_args_parse_2(config_paths: List[str]) -> None:
     Test multiple --config-path instances, --config file vs --arg, --var=val
     overrides, $var templates, option args, --random-init, etc.
     """
+    if sys.platform == 'win32':
+        os.environ['PWD'] = os.getcwd()
+
     config_file = 'cli/test-cli-config.jsonc'
     cli_args = ' '.join([f"--config-path {config_path}" for config_path in config_paths]) + \
         f' --config {config_file}' + \
@@ -93,6 +104,8 @@ def test_launcher_args_parse_2(config_paths: List[str]) -> None:
     assert launcher.global_config['testVmName'] == 'MockeryExperiment-vm'
     # Check that secondary expansion also works.
     assert launcher.global_config['testVnetName'] == 'MockeryExperiment-vm-vnet'
+    # Check that we can expand a $var in a config file that references an environment variable.
+    assert launcher.global_config["varWithEnvVarRef"] == path_join(os.getcwd(), "foo", abs_path=True)
     assert not launcher.teardown
 
     config = launcher.config_loader.load_config(config_file, ConfigSchema.CLI)

mlos_bench/mlos_bench/tests/services/local/local_exec_test.py
@@ -6,6 +6,7 @@
 Unit tests for the service to run the scripts locally.
 """
 import sys
+import tempfile
 
 import pytest
 import pandas
@@ -201,3 +202,24 @@ def test_run_script_middle_fail_pass(local_exec_service: LocalExecService) -> No
         "hello",
         "world",
     ]
+
+
+def test_temp_dir_path_expansion() -> None:
+    """
+    Test that we can control the temp_dir path using globals expansion.
+    """
+    # Create a temp dir for the test.
+    # Normally this would be a real path set on the CLI or in a global config,
+    # but for test purposes we still want it to be dynamic and cleaned up after
+    # the fact.
+    with tempfile.TemporaryDirectory() as temp_dir:
+        global_config = {
+            "workdir": temp_dir,   # e.g., "." or "/tmp/mlos_bench"
+        }
+        config = {
+            # The temp_dir for the LocalExecService should get expanded via workdir global config.
+            "temp_dir": "$workdir/temp",
+        }
+        local_exec_service = LocalExecService(config, global_config, parent=ConfigPersistenceService())
+        # pylint: disable=protected-access
+        assert local_exec_service._temp_dir == path_join(temp_dir, "temp")

mlos_bench/mlos_bench/launcher.py
@@ -12,6 +12,7 @@ command line.
 
 import argparse
 import logging
+import os
 import sys
 
 from string import Template
@@ -251,7 +252,14 @@ class Launcher:
         NOTE: `self.global_config` must be set.
         """
         if isinstance(value, str):
-            return Template(value).safe_substitute(self.global_config)
+            # use values either from the environment or from the global config
+            # Note: python 3.8 doesn't support the | operator, so we create a
+            # new set of params explicitly.
+            # Since this operates by updating the global_config along the way,
+            # we need to continually update the set of params to use for substitution.
+            params = dict(os.environ)
+            params.update(self.global_config)
+            return Template(value).safe_substitute(params)
         if isinstance(value, dict):
             # Note: we use a loop instead of dict comprehension in order to
             # allow secondary expansion of subsequent values immediately.

mlos_bench/mlos_bench/services/local/local_exec.py
@@ -13,6 +13,7 @@ import shlex
 import subprocess
 import sys
 
+from string import Template
 from typing import Any, Dict, Iterable, List, Mapping, Optional, Tuple, TYPE_CHECKING
 
 from mlos_bench.services.base_service import Service
@@ -201,6 +202,9 @@ class LocalExecService(TempDirContextService, SupportsLocalExec):
                 cmd = [" ".join(cmd)]
 
             _LOG.info("Run: %s", cmd)
+            if _LOG.isEnabledFor(logging.DEBUG):
+                _LOG.debug("Expands to: %s", Template(" ".join(cmd)).safe_substitute(env))
+                _LOG.debug("Current working dir: %s", cwd)
 
             proc = subprocess.run(cmd, env=env or None, cwd=cwd, shell=True,
                                   text=True, check=False, capture_output=True)

mlos_bench/mlos_bench/services/local/temp_dir_context.py
@@ -8,7 +8,9 @@ Helper functions to work with temp files locally on the scheduler side.
 
 import abc
 import logging
+import os
 from contextlib import nullcontext
+from string import Template
 from tempfile import TemporaryDirectory
 from typing import Any, Dict, Optional, Union
 
@@ -46,6 +48,12 @@ class TempDirContextService(Service, metaclass=abc.ABCMeta):
         """
         super().__init__(config, global_config, parent)
         self._temp_dir = self.config.get("temp_dir")
+        if self._temp_dir:
+            # expand globals
+            self._temp_dir = Template(self._temp_dir).safe_substitute(global_config or {})
+            # and resolve the path to absolute path
+            self._temp_dir = self._config_loader_service.resolve_path(self._temp_dir)
+        _LOG.info("%s: temp dir: %s", self, self._temp_dir)
         self.register([self.temp_dir_context])
 
     def temp_dir_context(self, path: Optional[str] = None) -> Union[TemporaryDirectory, nullcontext]:
@@ -62,6 +70,8 @@ class TempDirContextService(Service, metaclass=abc.ABCMeta):
         temp_dir_context : TemporaryDirectory
             Temporary directory context to use in the `with` clause.
         """
-        if path is None and self._temp_dir is None:
+        temp_dir = path or self._temp_dir
+        if temp_dir is None:
             return TemporaryDirectory()
-        return nullcontext(path or self._temp_dir)
+        os.makedirs(temp_dir, exist_ok=True)
+        return nullcontext(temp_dir)

mlos_bench/mlos_bench/tests/config/globals/global_test_config.jsonc
@@ -12,6 +12,8 @@
     "testVmName": "$experiment_id-vm",
     "testVnetName": "$testVmName-vnet",
 
+    "varWithEnvVarRef": "$PWD/foo",
+
     // Override the default value of the "max_iterations" parameter
     // of the optimizer when running local tests:
     "max_iterations": 5

mlos_bench/mlos_bench/tests/services/config_persistence_test.py
@@ -12,6 +12,7 @@ import pytest
 
 from mlos_bench.config.schemas import ConfigSchema
 from mlos_bench.services.config_persistence import ConfigPersistenceService
+from mlos_bench.util import path_join
 
 
 if sys.version_info < (3, 9):
@@ -31,11 +32,37 @@ def config_persistence_service() -> ConfigPersistenceService:
     return ConfigPersistenceService({
         "config_path": [
             "./non-existent-dir/test/foo/bar",                      # Non-existent config path
+            ".",                                                    # cwd
             str(files("mlos_bench.tests.config").joinpath("")),     # Test configs (relative to mlos_bench/tests)
             # Shouldn't be necessary since we automatically add this.
             # str(files("mlos_bench.config").joinpath("")),         # Stock configs
         ]
     })
+
+
+def test_cwd_in_explicit_search_path(config_persistence_service: ConfigPersistenceService) -> None:
+    """
+    Check that CWD is in the search path in the correct place.
+    """
+    # pylint: disable=protected-access
+    assert config_persistence_service._config_path is not None
+    cwd = path_join(os.getcwd(), abs_path=True)
+    assert config_persistence_service._config_path.index(cwd) == 1
+    with pytest.raises(ValueError):
+        config_persistence_service._config_path.index(cwd, 2)
+
+
+def test_cwd_in_default_search_path() -> None:
+    """
+    Checks that the CWD is prepended to the search path if not explicitly present.
+    """
+    # pylint: disable=protected-access
+    config_persistence_service = ConfigPersistenceService()
+    assert config_persistence_service._config_path is not None
+    cwd = path_join(os.getcwd(), abs_path=True)
+    assert config_persistence_service._config_path.index(cwd) == 0
+    with pytest.raises(ValueError):
+        config_persistence_service._config_path.index(cwd, 1)
 
 
 def test_resolve_stock_path(config_persistence_service: ConfigPersistenceService) -> None:

commit 7f9932432bb63e1e5450df9373cb58bb6c2743f0

Canonicalize the path for test comparison purposes (#531)
Fixup to Windows test for bug introduced by #529

Need to canonicalize paths to use `/` instead of `\\` when doing
comparisons.

mlos_bench/mlos_bench/services/config_persistence.py
@@ -69,7 +69,7 @@ class ConfigPersistenceService(Service, SupportsConfigLoading):
             if path not in self._config_path:
                 self._config_path.append(path_join(path, abs_path=True))
         # Prepend the cwd if not already on the list.
-        cwd = os.path.abspath(os.getcwd())
+        cwd = path_join(os.getcwd(), abs_path=True)
         if cwd not in self._config_path:
             self._config_path.insert(0, cwd)
         # Append the built-in config path if not already on the list.

mlos_bench/mlos_bench/tests/launcher_parse_args_test.py
@@ -70,7 +70,7 @@ def test_launcher_args_parse_1(config_paths: List[str]) -> None:
     # Check overriding values in a file from the command line.
     assert launcher.global_config['test_global_value_2'] == 'from-args'
     # Check that we can expand a $var in a config file that references an environment variable.
-    assert launcher.global_config["varWithEnvVarRef"] == path_join(os.getcwd(), "foo", abs_path=True)
+    assert path_join(launcher.global_config["varWithEnvVarRef"], abs_path=True) == path_join(os.getcwd(), "foo", abs_path=True)
     assert launcher.teardown
     # Check that the environment that got loaded looks to be of the right type.
     env_config = launcher.config_loader.load_config(env_conf_path, ConfigSchema.ENVIRONMENT)
@@ -105,7 +105,7 @@ def test_launcher_args_parse_2(config_paths: List[str]) -> None:
     # Check that secondary expansion also works.
     assert launcher.global_config['testVnetName'] == 'MockeryExperiment-vm-vnet'
     # Check that we can expand a $var in a config file that references an environment variable.
-    assert launcher.global_config["varWithEnvVarRef"] == path_join(os.getcwd(), "foo", abs_path=True)
+    assert path_join(launcher.global_config["varWithEnvVarRef"], abs_path=True) == path_join(os.getcwd(), "foo", abs_path=True)
     assert not launcher.teardown
 
     config = launcher.config_loader.load_config(config_file, ConfigSchema.CLI)
@@ -133,3 +133,7 @@ def test_launcher_args_parse_2(config_paths: List[str]) -> None:
     # applies to a temporary Optimizer used to populate the initial values via
     # random sampling.
     # assert launcher.optimizer.seed == 1234
+
+
+if __name__ == '__main__':
+    pytest.main([__file__, "-n1"])

mlos_bench/mlos_bench/tests/services/local/local_exec_test.py
@@ -222,4 +222,5 @@ def test_temp_dir_path_expansion() -> None:
         }
         local_exec_service = LocalExecService(config, global_config, parent=ConfigPersistenceService())
         # pylint: disable=protected-access
-        assert local_exec_service._temp_dir == path_join(temp_dir, "temp")
+        assert isinstance(local_exec_service._temp_dir, str)
+        assert path_join(local_exec_service._temp_dir, abs_path=True) == path_join(temp_dir, "temp", abs_path=True)