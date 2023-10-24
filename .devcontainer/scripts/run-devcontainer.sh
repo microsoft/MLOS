a1edebf847bcec006bdfbd6294adc63bb4b900dc
Brian Kroth <bpkroth@microsoft.com>
2023-10-24 17:07:38 +0000

    change the way we add the docker group so we retain supplementary groups

--- .devcontainer/scripts/run-devcontainer.sh
+++ .devcontainer/scripts/run-devcontainer.sh
@@ -31,7 +31,8 @@ fi
 mkdir -p "/tmp/$container_name/dc/shellhistory"
 docker run -it --rm \
     --name "$container_name" \
-    --user vscode:$docker_gid \
+    --user vscode \
+    --group-add $docker_gid \
     -v "$HOME/.azure":/dc/azure \
     -v "/tmp/$container_name/dc/shellhistory:/dc/shellhistory" \
     -v /var/run/docker.sock:/var/run/docker.sock \
