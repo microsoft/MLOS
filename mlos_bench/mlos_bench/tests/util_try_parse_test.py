3b4f82e5821cc40fff9b116b817c9de3629a1059
Brian Kroth <bpkroth@microsoft.com>
2023-10-27 21:22:19 +0000

    actually add tests

---
+++ mlos_bench/mlos_bench/tests/util_try_parse_test.py
@@ -0,0 +1,20 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+"""
+Unit tests for try_parse_val utility function.
+"""
+
+from mlos_bench.util import try_parse_val
+
+
+def test_try_parse_val() -> None:
+    """
+    Check that we can retrieve git info about the current repository correctly.
+    """
+    assert try_parse_val(None) is None
+    assert try_parse_val("1") == int(1)
+    assert try_parse_val("1.1") == float(1.1)
+    assert try_parse_val("1e6") == float(1e6)
+    assert try_parse_val("str") == str("str")
