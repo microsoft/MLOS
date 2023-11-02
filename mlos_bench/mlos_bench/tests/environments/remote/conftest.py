2024fe2c4b36c88f2b4e1db8b180f8e72b24b95c
Brian Kroth <bpkroth@microsoft.com>
2023-11-02 15:04:56 +0000

    remove an unused fixture

--- mlos_bench/mlos_bench/tests/environments/remote/conftest.py
+++ mlos_bench/mlos_bench/tests/environments/remote/conftest.py
@@ -11,6 +11,5 @@ import mlos_bench.tests.services.remote.ssh.fixtures as ssh_fixtures
 # Expose those as local names.
 ssh_test_server_hostname = ssh_fixtures.ssh_test_server_hostname
 ssh_test_server = ssh_fixtures.ssh_test_server
-alt_test_server = ssh_fixtures.alt_test_server
 ssh_host_service = ssh_fixtures.ssh_host_service
 ssh_fileshare_service = ssh_fixtures.ssh_fileshare_service
