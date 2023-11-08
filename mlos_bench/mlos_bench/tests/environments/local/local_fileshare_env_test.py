5f56c462edbb96422c3341643b007d06a72528a6
Sergiy Matusevych <sergiym@microsoft.com>
2023-11-08 12:06:21 -0800

    Add unit tests for LocalFileShareEnv $var expansion in upload and download paths (#587)

---
+++ mlos_bench/mlos_bench/tests/environments/local/local_fileshare_env_test.py
@@ -0,0 +1,95 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+"""
+Unit tests for passing shell environment variables into LocalEnv scripts.
+"""
+import pytest
+
+from mlos_bench.tunables.tunable_groups import TunableGroups
+from mlos_bench.environments.local.local_fileshare_env import LocalFileShareEnv
+from mlos_bench.services.config_persistence import ConfigPersistenceService
+from mlos_bench.services.local.local_exec import LocalExecService
+
+from mlos_bench.tests.services.remote.mock.mock_fileshare_service import MockFileShareService
+
+# pylint: disable=redefined-outer-name
+
+
+@pytest.fixture(scope="module")
+def mock_fileshare_service() -> MockFileShareService:
+    """
+    Create a new mock FileShareService instance.
+    """
+    return MockFileShareService(
+        config={"fileShareName": "MOCK_FILESHARE"},
+        parent=LocalExecService(parent=ConfigPersistenceService())
+    )
+
+
+@pytest.fixture
+def local_fileshare_env(tunable_groups: TunableGroups,
+                        mock_fileshare_service: MockFileShareService) -> LocalFileShareEnv:
+    """
+    Create a LocalFileShareEnv instance.
+    """
+    env = LocalFileShareEnv(
+        name="TestLocalFileShareEnv",
+        config={
+            "const_args": {
+                "experiment_id": "EXP_ID",  # Passed into "shell_env_params"
+                "trial_id": 222,            # NOT passed into "shell_env_params"
+            },
+            "tunable_params": ["boot"],
+            "shell_env_params": [
+                "trial_id",                 # From "const_arg"
+                "idle",                     # From "tunable_params", == "halt"
+            ],
+            "upload": [
+                {
+                    "from": "grub.cfg",
+                    "to": "$experiment_id/$trial_id/input/grub.cfg",
+                },
+                {
+                    "from": "data_$idle.csv",
+                    "to": "$experiment_id/$trial_id/input/data_$idle.csv",
+                },
+            ],
+            "run": [
+                "echo No-op run"
+            ],
+            "download": [
+                {
+                    "from": "$experiment_id/$trial_id/$idle/data.csv",
+                    "to": "output/data_$idle.csv",
+                },
+            ],
+        },
+        tunables=tunable_groups,
+        service=mock_fileshare_service,
+    )
+    return env
+
+
+def test_local_fileshare_env(tunable_groups: TunableGroups,
+                             mock_fileshare_service: MockFileShareService,
+                             local_fileshare_env: LocalFileShareEnv) -> None:
+    """
+    Test that the LocalFileShareEnv correctly expands the `$VAR` variables
+    in the upload and download sections of the config.
+    """
+    with local_fileshare_env as env_context:
+        assert env_context.setup(tunable_groups)
+        (status, _) = env_context.run()
+        assert status.is_succeeded()
+        assert mock_fileshare_service.get_upload() == [
+            ("grub.cfg", "EXP_ID/222/input/grub.cfg"),
+            ("data_halt.csv", "EXP_ID/222/input/data_halt.csv"),
+        ]
+        # NOTE: The "download" section is run twice -- once to check
+        # the status of the run, and once to get the final results.
+        assert mock_fileshare_service.get_download() == [
+            ("EXP_ID/222/halt/data.csv", "output/data_halt.csv"),
+            ("EXP_ID/222/halt/data.csv", "output/data_halt.csv"),
+        ]
--- mlos_bench/mlos_bench/tests/services/remote/mock/mock_fileshare_service.py
+++ mlos_bench/mlos_bench/tests/services/remote/mock/mock_fileshare_service.py
@@ -7,7 +7,7 @@ A collection Service functions for mocking file share ops.
 """
 
 import logging
-from typing import Any, Callable, Dict, List, Optional, Union
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 from mlos_bench.services.base_service import Service
 from mlos_bench.services.base_fileshare import FileShareService
@@ -29,9 +29,23 @@ class MockFileShareService(FileShareService, SupportsFileShareOps):
             config, global_config, parent,
             self.merge_methods(methods, [self.upload, self.download])
         )
-
-    def download(self, params: dict, remote_path: str, local_path: str, recursive: bool = True) -> None:
-        pass
+        self._upload: List[Tuple[str, str]] = []
+        self._download: List[Tuple[str, str]] = []
 
     def upload(self, params: dict, local_path: str, remote_path: str, recursive: bool = True) -> None:
-        pass
+        self._upload.append((local_path, remote_path))
+
+    def download(self, params: dict, remote_path: str, local_path: str, recursive: bool = True) -> None:
+        self._download.append((remote_path, local_path))
+
+    def get_upload(self) -> List[Tuple[str, str]]:
+        """
+        Get the list of files that were uploaded.
+        """
+        return self._upload
+
+    def get_download(self) -> List[Tuple[str, str]]:
+        """
+        Get the list of files that were downloaded.
+        """
+        return self._download
