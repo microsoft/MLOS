6a17809f2308d9766a43c4223ae12b5e6dc99898
Brian Kroth <bpkroth@microsoft.com>
2023-11-20 22:09:54 +0000

    add basic network environment

--- mlos_bench/mlos_bench/environments/remote/__init__.py
+++ mlos_bench/mlos_bench/environments/remote/__init__.py
@@ -9,6 +9,7 @@ Remote Tunable Environments for mlos_bench.
 from mlos_bench.environments.remote.remote_env import RemoteEnv
 from mlos_bench.environments.remote.os_env import OSEnv
 from mlos_bench.environments.remote.host_env import HostEnv
+from mlos_bench.environments.remote.network_env import NetworkEnv
 from mlos_bench.environments.remote.vm_env import VMEnv
 from mlos_bench.environments.remote.saas_env import SaaSEnv
 
@@ -16,6 +17,7 @@ __all__ = [
     'RemoteEnv',
     'OSEnv',
     'HostEnv',
+    'NetworkEnv',
     'VMEnv',
     'SaaSEnv',
 ]
