1dba055a1479c0e578a2db881a1f48cd2f313a42
Brian Kroth <bpkroth@microsoft.com>
2023-11-20 22:08:43 +0000

    add network provisioner type

--- mlos_bench/mlos_bench/services/types/__init__.py
+++ mlos_bench/mlos_bench/services/types/__init__.py
@@ -9,6 +9,7 @@ Service types for implementing declaring Service behavior for Environments to us
 from mlos_bench.services.types.config_loader_type import SupportsConfigLoading
 from mlos_bench.services.types.fileshare_type import SupportsFileShareOps
 from mlos_bench.services.types.host_provisioner_type import SupportsHostProvisioning
+from mlos_bench.services.types.network_provisioner_type import SupportsNetworkProvisioning
 from mlos_bench.services.types.local_exec_type import SupportsLocalExec
 from mlos_bench.services.types.remote_exec_type import SupportsRemoteExec
 from mlos_bench.services.types.remote_config_type import SupportsRemoteConfig
@@ -18,6 +19,7 @@ __all__ = [
     'SupportsConfigLoading',
     'SupportsFileShareOps',
     'SupportsHostProvisioning',
+    'SupportsNetworkProvisioning',
     'SupportsLocalExec',
     'SupportsRemoteExec',
     'SupportsRemoteConfig',
