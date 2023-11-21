b0edbeaf8815cfbe89087b47572afe3e17280dda
Brian Kroth <bpkroth@microsoft.com>
2023-11-21 17:07:45 +0000

    sort

--- mlos_bench/mlos_bench/environments/remote/__init__.py
+++ mlos_bench/mlos_bench/environments/remote/__init__.py
@@ -6,18 +6,18 @@
 Remote Tunable Environments for mlos_bench.
 """
 
-from mlos_bench.environments.remote.remote_env import RemoteEnv
-from mlos_bench.environments.remote.os_env import OSEnv
 from mlos_bench.environments.remote.host_env import HostEnv
 from mlos_bench.environments.remote.network_env import NetworkEnv
-from mlos_bench.environments.remote.vm_env import VMEnv
+from mlos_bench.environments.remote.os_env import OSEnv
+from mlos_bench.environments.remote.remote_env import RemoteEnv
 from mlos_bench.environments.remote.saas_env import SaaSEnv
+from mlos_bench.environments.remote.vm_env import VMEnv
 
 __all__ = [
-    'RemoteEnv',
-    'OSEnv',
     'HostEnv',
     'NetworkEnv',
-    'VMEnv',
+    'OSEnv',
+    'RemoteEnv',
     'SaaSEnv',
+    'VMEnv',
 ]
