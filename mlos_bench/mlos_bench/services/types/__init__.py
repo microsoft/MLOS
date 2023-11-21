39bec810a1df1ef53f0590075a21ea326a4ab138
Brian Kroth <bpkroth@microsoft.com>
2023-11-21 17:09:05 +0000

    sort

--- mlos_bench/mlos_bench/services/types/__init__.py
+++ mlos_bench/mlos_bench/services/types/__init__.py
@@ -9,18 +9,18 @@ Service types for implementing declaring Service behavior for Environments to us
 from mlos_bench.services.types.config_loader_type import SupportsConfigLoading
 from mlos_bench.services.types.fileshare_type import SupportsFileShareOps
 from mlos_bench.services.types.host_provisioner_type import SupportsHostProvisioning
-from mlos_bench.services.types.network_provisioner_type import SupportsNetworkProvisioning
 from mlos_bench.services.types.local_exec_type import SupportsLocalExec
-from mlos_bench.services.types.remote_exec_type import SupportsRemoteExec
+from mlos_bench.services.types.network_provisioner_type import SupportsNetworkProvisioning
 from mlos_bench.services.types.remote_config_type import SupportsRemoteConfig
+from mlos_bench.services.types.remote_exec_type import SupportsRemoteExec
 
 
 __all__ = [
     'SupportsConfigLoading',
     'SupportsFileShareOps',
     'SupportsHostProvisioning',
-    'SupportsNetworkProvisioning',
     'SupportsLocalExec',
-    'SupportsRemoteExec',
+    'SupportsNetworkProvisioning',
     'SupportsRemoteConfig',
+    'SupportsRemoteExec',
 ]
