a152c617b0ffa37e43d313ace2f745d9f6bd324b
Brian Kroth <bpkroth@microsoft.com>
2023-11-29 22:01:50 +0000

    stubbing out an explicit test file

---
+++ mlos_bench/mlos_bench/tests/dict_templater_test.py
@@ -0,0 +1,25 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+"""
+Unit tests for DictTemplater class.
+"""
+
+from mlos_bench.dict_templater import DictTemplater
+
+
+def test_no_side_effects() -> None:
+    raise NotImplementedError("TODO")
+
+
+def test_secondary_expansion() -> None:
+    raise NotImplementedError("TODO")
+
+
+def test_from_os_env_expansion() -> None:
+    raise NotImplementedError("TODO")
+
+
+def test_from_extras_expansion() -> None:
+    raise NotImplementedError("TODO")
