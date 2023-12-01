microsoft/MLOS

commit 3b9d0b443aca83aa5ff3ff83af85ef96a24a4e8e

Add support for recursive dictionary templating to more places (#614)
Fixes extracted from #606 

This expands support for doing secondary `$var` string template
expansion to two additional places beyond `global_config`:

- `const_args`
- `deploymentTemplateParameters` in `AzureVmService` (and soon
`AzureNetworkService`)

Expands a few test cases to check for this and relies on one prior
existing one to still pass (the `launch_parse_args` tests which already
checked for this behavior in `global_config` expansion).

mlos_bench/mlos_bench/dict_templater.py
@@ -1,0 +1,82 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+"""
+Simple class to help with nested dictionary $var templating.
+"""
+
+from copy import deepcopy
+from string import Template
+from typing import Any, Dict, Optional
+
+import os
+
+
+class DictTemplater:    # pylint: disable=too-few-public-methods
+    """
+    Simple class to help with nested dictionary $var templating.
+    """
+
+    def __init__(self, source_dict: Dict[str, Any]):
+        """
+        Initialize the templater.
+
+        Parameters
+        ----------
+        source_dict : Dict[str, Any]
+            The template dict to use for source variables.
+        """
+        # A copy of the initial data structure we were given with templates intact.
+        self._template_dict = deepcopy(source_dict)
+        # The source/target dictionary to expand.
+        self._dict: Dict[str, Any] = {}
+
+    def expand_vars(self, *,
+                    extra_source_dict: Optional[Dict[str, Any]] = None,
+                    use_os_env: bool = False) -> Dict[str, Any]:
+        """
+        Expand the template variables in the destination dictionary.
+
+        Parameters
+        ----------
+        extra_source_dict : Dict[str, Any]
+            An optional extra source dictionary to use for expansion.
+        use_os_env : bool
+            Whether to use the os environment variables a final fallback for expansion.
+
+        Returns
+        -------
+        Dict[str, Any]
+            The expanded dictionary.
+        """
+        self._dict = deepcopy(self._template_dict)
+        self._dict = self._expand_vars(self._dict, extra_source_dict, use_os_env)
+        assert isinstance(self._dict, dict)
+        return self._dict
+
+    def _expand_vars(self, value: Any, extra_source_dict: Optional[Dict[str, Any]], use_os_env: bool) -> Any:
+        """
+        Recursively expand $var strings in the currently operating dictionary.
+        """
+        if isinstance(value, str):
+            # First try to expand all $vars internally.
+            value = Template(value).safe_substitute(self._dict)
+            # Next, if there are any left, try to expand them from the extra source dict.
+            if extra_source_dict:
+                value = Template(value).safe_substitute(extra_source_dict)
+            # Finally, fallback to the os environment.
+            if use_os_env:
+                value = Template(value).safe_substitute(dict(os.environ))
+        elif isinstance(value, dict):
+            # Note: we use a loop instead of dict comprehension in order to
+            # allow secondary expansion of subsequent values immediately.
+            for (key, val) in value.items():
+                value[key] = self._expand_vars(val, extra_source_dict, use_os_env)
+        elif isinstance(value, list):
+            value = [self._expand_vars(val, extra_source_dict, use_os_env) for val in value]
+        elif isinstance(value, (int, float, bool)) or value is None:
+            return value
+        else:
+            raise ValueError(f"Unexpected type {type(value)} for value {value}")
+        return value

mlos_bench/mlos_bench/tests/dict_templater_test.py
@@ -1,0 +1,149 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+"""
+Unit tests for DictTemplater class.
+"""
+
+from copy import deepcopy
+from typing import Any, Dict
+
+import os
+
+import pytest
+
+from mlos_bench.dict_templater import DictTemplater
+
+
+@pytest.fixture
+def source_template_dict() -> Dict[str, Any]:
+    """A source dictionary with template variables."""
+    return {
+        "extra_str-ref": "$extra_str-ref",
+        "str": "string",
+        "str_ref": "$str-ref",
+        "secondary_expansion": "${str_ref}",
+        "tertiary_expansion": "$secondary_expansion",
+        "int": 1,
+        "int_ref": "$int-ref",
+        "float": 1.0,
+        "float_ref": "$float-ref",
+        "bool": True,
+        "bool_ref": "$bool-ref",
+        "list": [
+            "$str",
+            "$int",
+            "$float",
+        ],
+        "dict": {
+            "nested-str-ref": "nested-$str-ref",
+            "nested-extra-str-ref": "nested-$extra_str-ref",
+        },
+    }
+
+
+# pylint: disable=redefined-outer-name
+
+
+def test_no_side_effects(source_template_dict: Dict[str, Any]) -> None:
+    """
+    Test that the templater does not modify the source dictionary.
+    """
+    source_template_dict_copy = deepcopy(source_template_dict)
+    results = DictTemplater(source_template_dict_copy).expand_vars()
+    assert results
+    assert source_template_dict_copy == source_template_dict
+
+
+def test_secondary_expansion(source_template_dict: Dict[str, Any]) -> None:
+    """
+    Test that internal expansions work as expected.
+    """
+    results = DictTemplater(source_template_dict).expand_vars()
+    assert results == {
+        "extra_str-ref": "$extra_str-ref",
+        "str": "string",
+        "str_ref": "string-ref",
+        "secondary_expansion": "string-ref",
+        "tertiary_expansion": "string-ref",
+        "int": 1,
+        "int_ref": "1-ref",
+        "float": 1.0,
+        "float_ref": "1.0-ref",
+        "bool": True,
+        "bool_ref": "True-ref",
+        "list": [
+            "string",
+            "1",
+            "1.0",
+        ],
+        "dict": {
+            "nested-str-ref": "nested-string-ref",
+            "nested-extra-str-ref": "nested-$extra_str-ref",
+        },
+    }
+
+
+def test_os_env_expansion(source_template_dict: Dict[str, Any]) -> None:
+    """
+    Test that expansions from OS env work as expected.
+    """
+    os.environ["extra_str"] = "os-env-extra_str"
+    os.environ["string"] = "shouldn't be used"
+    results = DictTemplater(source_template_dict).expand_vars(use_os_env=True)
+    assert results == {
+        "extra_str-ref": f"{os.environ['extra_str']}-ref",
+        "str": "string",
+        "str_ref": "string-ref",
+        "secondary_expansion": "string-ref",
+        "tertiary_expansion": "string-ref",
+        "int": 1,
+        "int_ref": "1-ref",
+        "float": 1.0,
+        "float_ref": "1.0-ref",
+        "bool": True,
+        "bool_ref": "True-ref",
+        "list": [
+            "string",
+            "1",
+            "1.0",
+        ],
+        "dict": {
+            "nested-str-ref": "nested-string-ref",
+            "nested-extra-str-ref": f"nested-{os.environ['extra_str']}-ref",
+        },
+    }
+
+
+def test_from_extras_expansion(source_template_dict: Dict[str, Any]) -> None:
+    """
+    Test that
+    """
+    extra_source_dict = {
+        "extra_str": "str-from-extras",
+        "string": "shouldn't be used",
+    }
+    results = DictTemplater(source_template_dict).expand_vars(extra_source_dict=extra_source_dict)
+    assert results == {
+        "extra_str-ref": f"{extra_source_dict['extra_str']}-ref",
+        "str": "string",
+        "str_ref": "string-ref",
+        "secondary_expansion": "string-ref",
+        "tertiary_expansion": "string-ref",
+        "int": 1,
+        "int_ref": "1-ref",
+        "float": 1.0,
+        "float_ref": "1.0-ref",
+        "bool": True,
+        "bool_ref": "True-ref",
+        "list": [
+            "string",
+            "1",
+            "1.0",
+        ],
+        "dict": {
+            "nested-str-ref": "nested-string-ref",
+            "nested-extra-str-ref": f"nested-{extra_source_dict['extra_str']}-ref",
+        },
+    }

mlos_bench/mlos_bench/environments/base_environment.py
@@ -10,12 +10,12 @@ import abc
 import json
 import logging
 from datetime import datetime
-from string import Template
 from types import TracebackType
 from typing import Any, Dict, Iterable, List, Optional, Sequence, Tuple, Type, TYPE_CHECKING, Union
 from typing_extensions import Literal
 
 from mlos_bench.config.schemas import ConfigSchema
+from mlos_bench.dict_templater import DictTemplater
 from mlos_bench.environments.status import Status
 from mlos_bench.services.base_service import Service
 from mlos_bench.tunables.tunable import TunableValue
@@ -201,10 +201,7 @@ class Environment(metaclass=abc.ABCMeta):
         """
         Expand `$var` into actual values of the variables.
         """
-        return {
-            key: Template(val).safe_substitute(global_config) if isinstance(val, str) else val
-            for key, val in params.items()
-        }
+        return DictTemplater(params).expand_vars(extra_source_dict=global_config)
 
     @property
     def _config_loader_service(self) -> "SupportsConfigLoading":

mlos_bench/mlos_bench/launcher.py
@@ -12,13 +12,12 @@ command line.
 
 import argparse
 import logging
-import os
 import sys
 
-from string import Template
 from typing import Any, Dict, Iterable, List, Optional, Tuple, Type
 
 from mlos_bench.config.schemas import ConfigSchema
+from mlos_bench.dict_templater import DictTemplater
 from mlos_bench.util import BaseTypeVar, try_parse_val
 
 from mlos_bench.tunables.tunable import TunableValue
@@ -103,7 +102,7 @@ class Launcher:
         # It's useful to keep it there explicitly mostly for the --help output.
         if args.experiment_id:
             self.global_config['experiment_id'] = args.experiment_id
-        self.global_config = self._expand_vars(self.global_config)
+        self.global_config = DictTemplater(self.global_config).expand_vars(use_os_env=True)
         assert isinstance(self.global_config, dict)
         # Ensure that the trial_id is present since it gets used by some other
         # configs but is typically controlled by the run optimize loop.
@@ -296,30 +295,6 @@ class Launcher:
         if config_path:
             global_config["config_path"] = config_path
         return global_config
-
-    def _expand_vars(self, value: Any) -> Any:
-        """
-        Expand dollar variables in the globals.
-
-        NOTE: `self.global_config` must be set.
-        """
-        if isinstance(value, str):
-            # use values either from the environment or from the global config
-            # Note: python 3.8 doesn't support the | operator, so we create a
-            # new set of params explicitly.
-            # Since this operates by updating the global_config along the way,
-            # we need to continually update the set of params to use for substitution.
-            params = self.global_config.copy()
-            params.update(dict(os.environ))
-            return Template(value).safe_substitute(params)
-        if isinstance(value, dict):
-            # Note: we use a loop instead of dict comprehension in order to
-            # allow secondary expansion of subsequent values immediately.
-            for (key, val) in value.items():
-                value[key] = self._expand_vars(val)
-        if isinstance(value, list):
-            return [self._expand_vars(val) for val in value]
-        return value
 
     def _init_tunable_values(self, random_init: bool, seed: Optional[int],
                              args_tunables: Optional[str]) -> TunableGroups:

mlos_bench/mlos_bench/services/remote/azure/azure_vm_services.py
@@ -15,6 +15,7 @@ from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Union
 import requests
 from requests.adapters import HTTPAdapter, Retry
 
+from mlos_bench.dict_templater import DictTemplater
 from mlos_bench.environments.status import Status
 from mlos_bench.services.base_service import Service
 from mlos_bench.services.types.authenticator_type import SupportsAuth
@@ -191,8 +192,10 @@ class AzureVMService(Service, SupportsHostProvisioning, SupportsHostOps, Support
         assert template is not None and isinstance(template, dict)
         self._deploy_template = template
 
-        self._deploy_params = merge_parameters(
-            dest=self.config['deploymentTemplateParameters'].copy(), source=global_config)
+        # Allow for recursive variable expansion as we do with global params and const_args.
+        deploy_params = DictTemplater(self.config['deploymentTemplateParameters']).expand_vars(extra_source_dict=global_config)
+
+        self._deploy_params = merge_parameters(dest=deploy_params, source=global_config)
 
         # As a convenience, allow reading customData out of a file, rather than
         # embedding it in a json config file.
@@ -205,6 +208,13 @@ class AzureVMService(Service, SupportsHostProvisioning, SupportsHostOps, Support
             self._custom_data_file = self.config_loader_service.resolve_path(self._custom_data_file)
             with open(self._custom_data_file, 'r', encoding='utf-8') as custom_data_fh:
                 self._deploy_params["customData"] = custom_data_fh.read()
+
+    @property
+    def deploy_params(self) -> dict:
+        """
+        Get the deployment parameters.
+        """
+        return self._deploy_params
 
     def _get_session(self, params: dict) -> requests.Session:
         """

mlos_bench/mlos_bench/tests/environments/base_env_test.py
@@ -63,6 +63,7 @@ def test_expand_const_args() -> None:
         "a": "b",
         "foo": "$bar/baz",
         "1": 1,
+        "recursive": "$foo/expansion",
     }
     global_config: Dict[str, TunableValue] = {
         "bar": "blah",
@@ -72,4 +73,5 @@ def test_expand_const_args() -> None:
         "a": "b",
         "foo": "blah/baz",
         "1": 1,
+        "recursive": "blah/baz/expansion",
     }

mlos_bench/mlos_bench/tests/services/remote/azure/azure_services_test.py
@@ -70,6 +70,31 @@ def test_wait_host_deployment_retry(mock_getconn: MagicMock,
     assert status == operation_status
 
 
+def test_azure_vm_service_recursive_template_params(azure_auth_service: AzureAuthService) -> None:
+    """
+    Test expanding template params recursively.
+    """
+    config = {
+        "deploymentTemplatePath": "services/remote/azure/arm-templates/azuredeploy-ubuntu-vm.jsonc",
+        "deploymentName": "TEST_DEPLOYMENT1",
+        "subscription": "TEST_SUB1",
+        "resourceGroup": "TEST_RG1",
+        "deploymentTemplateParameters": {
+            "location": "$location",
+            "vmMeta": "$vmName-$location",
+            "vmNsg": "$vmMeta-nsg",
+        },
+    }
+    global_config = {
+        "vmName": "test-vm",
+        "location": "eastus",
+    }
+    azure_vm_service = AzureVMService(config, global_config, parent=azure_auth_service)
+    assert azure_vm_service.deploy_params["location"] == global_config["location"]
+    assert azure_vm_service.deploy_params["vmMeta"] == f'{global_config["vmName"]}-{global_config["location"]}'
+    assert azure_vm_service.deploy_params["vmNsg"] == f'{azure_vm_service.deploy_params["vmMeta"]}-nsg'
+
+
 def test_azure_vm_service_custom_data(azure_auth_service: AzureAuthService) -> None:
     """
     Test loading custom data from a file.
@@ -92,8 +117,7 @@ def test_azure_vm_service_custom_data(azure_auth_service: AzureAuthService) -> N
         config_with_custom_data['deploymentTemplateParameters']['customData'] = "DUMMY_CUSTOM_DATA"  # type: ignore[index]
         AzureVMService(config_with_custom_data, global_config, parent=azure_auth_service)
     azure_vm_service = AzureVMService(config, global_config, parent=azure_auth_service)
-    # pylint: disable=protected-access
-    assert azure_vm_service._deploy_params['customData']
+    assert azure_vm_service.deploy_params['customData']
 
 
 @pytest.mark.parametrize(

commit 67ccdac441175d8c9b74440a5b92bb0f5ca7db60

Abstract OS environment variable case sensitivity name handling across platforms (#618)
Fixes a bug introduced in #614 for Windows tests.

On Windows, 

```python
os.environ["string"] = "str"
```

internally gets replaced as

```python
os.environ["STRING"]
```

The workaround is to use `nt.environ` on Windows instead.

This change introduces an abstraction to use a new simple drop in
replacement library everywhere we need to handle environment variables.

mlos_bench/mlos_bench/dict_templater.py
@@ -10,7 +10,7 @@ from copy import deepcopy
 from string import Template
 from typing import Any, Dict, Optional
 
-import os
+from mlos_bench.os_environ import environ
 
 
 class DictTemplater:    # pylint: disable=too-few-public-methods
@@ -67,7 +67,7 @@ class DictTemplater:    # pylint: disable=too-few-public-methods
                 value = Template(value).safe_substitute(extra_source_dict)
             # Finally, fallback to the os environment.
             if use_os_env:
-                value = Template(value).safe_substitute(dict(os.environ))
+                value = Template(value).safe_substitute(dict(environ))
         elif isinstance(value, dict):
             # Note: we use a loop instead of dict comprehension in order to
             # allow secondary expansion of subsequent values immediately.

mlos_bench/mlos_bench/os_environ.py
@@ -1,0 +1,37 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+"""
+Simple platform agnostic abstraction for the OS environment variables.
+Meant as a replacement for os.environ vs nt.environ.
+
+Example
+-------
+from mlos_bench.os_env import environ
+environ['FOO'] = 'bar'
+environ.get('PWD')
+"""
+
+import os
+import sys
+
+
+if sys.platform == 'win32':
+    import nt   # type: ignore[import-not-found]    # pylint: disable=import-error  # (3.8)
+
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
+
+if sys.version_info >= (3, 9):
+    EnvironType: TypeAlias = os._Environ[str]   # pylint: disable=protected-access,disable=unsubscriptable-object
+else:
+    EnvironType: TypeAlias = os._Environ        # pylint: disable=protected-access
+
+# Handle case sensitivity differences between platforms.
+# https://stackoverflow.com/a/19023293
+environ: EnvironType = nt.environ if sys.platform == 'win32' else os.environ    # type: ignore[name-defined]
+
+__all__ = ['environ']

mlos_bench/mlos_bench/services/local/local_exec.py
@@ -18,6 +18,7 @@ from typing import (
     Any, Callable, Dict, Iterable, List, Mapping, Optional, Tuple, TYPE_CHECKING, Union
 )
 
+from mlos_bench.os_environ import environ
 from mlos_bench.services.base_service import Service
 from mlos_bench.services.local.temp_dir_context import TempDirContextService
 from mlos_bench.services.types.local_exec_type import SupportsLocalExec
@@ -199,7 +200,7 @@ class LocalExecService(TempDirContextService, SupportsLocalExec):
 
         if sys.platform == 'win32':
             # A hack to run Python on Windows with env variables set:
-            env_copy = os.environ.copy()
+            env_copy = environ.copy()
             env_copy["PYTHONPATH"] = ""
             env_copy.update(env)
             env = env_copy

mlos_bench/mlos_bench/tests/dict_templater_test.py
@@ -9,11 +9,10 @@ Unit tests for DictTemplater class.
 from copy import deepcopy
 from typing import Any, Dict
 
-import os
-
 import pytest
 
 from mlos_bench.dict_templater import DictTemplater
+from mlos_bench.os_environ import environ
 
 
 @pytest.fixture
@@ -89,11 +88,12 @@ def test_os_env_expansion(source_template_dict: Dict[str, Any]) -> None:
     """
     Test that expansions from OS env work as expected.
     """
-    os.environ["extra_str"] = "os-env-extra_str"
-    os.environ["string"] = "shouldn't be used"
+    environ["extra_str"] = "os-env-extra_str"
+    environ["string"] = "shouldn't be used"
+
     results = DictTemplater(source_template_dict).expand_vars(use_os_env=True)
     assert results == {
-        "extra_str-ref": f"{os.environ['extra_str']}-ref",
+        "extra_str-ref": f"{environ['extra_str']}-ref",
         "str": "string",
         "str_ref": "string-ref",
         "secondary_expansion": "string-ref",
@@ -111,7 +111,7 @@ def test_os_env_expansion(source_template_dict: Dict[str, Any]) -> None:
         ],
         "dict": {
             "nested-str-ref": "nested-string-ref",
-            "nested-extra-str-ref": f"nested-{os.environ['extra_str']}-ref",
+            "nested-extra-str-ref": f"nested-{environ['extra_str']}-ref",
         },
     }
 

mlos_bench/mlos_bench/tests/launcher_parse_args_test.py
@@ -16,6 +16,7 @@ import pytest
 
 from mlos_bench.launcher import Launcher
 from mlos_bench.optimizers import MockOptimizer
+from mlos_bench.os_environ import environ
 from mlos_bench.config.schemas import ConfigSchema
 from mlos_bench.util import path_join
 from mlos_bench.services.types import (
@@ -61,10 +62,10 @@ def test_launcher_args_parse_1(config_paths: List[str]) -> None:
     # changing into the code directory, but doesn't update the PWD environment
     # variable so we use a separate variable.
     # See global_test_config.jsonc for more details.
-    os.environ["CUSTOM_PATH_FROM_ENV"] = os.getcwd()
+    environ["CUSTOM_PATH_FROM_ENV"] = os.getcwd()
     if sys.platform == 'win32':
         # Some env tweaks for platform compatibility.
-        os.environ['USER'] = os.environ['USERNAME']
+        environ['USER'] = environ['USERNAME']
 
     # This is part of the minimal required args by the Launcher.
     env_conf_path = 'environments/mock/mock_env.jsonc'
@@ -113,10 +114,10 @@ def test_launcher_args_parse_2(config_paths: List[str]) -> None:
     # changing into the code directory, but doesn't update the PWD environment
     # variable so we use a separate variable.
     # See global_test_config.jsonc for more details.
-    os.environ["CUSTOM_PATH_FROM_ENV"] = os.getcwd()
+    environ["CUSTOM_PATH_FROM_ENV"] = os.getcwd()
     if sys.platform == 'win32':
         # Some env tweaks for platform compatibility.
-        os.environ['USER'] = os.environ['USERNAME']
+        environ['USER'] = environ['USERNAME']
 
     config_file = 'cli/test-cli-config.jsonc'
     cli_args = ' '.join([f"--config-path {config_path}" for config_path in config_paths]) + \