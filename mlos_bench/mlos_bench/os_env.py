2b3ed11a33ec502c0a287ff9d6a22e523809774a
Brian Kroth <bpkroth@microsoft.com>
2023-12-01 12:53:08 -0600

    docs

--- mlos_bench/mlos_bench/os_env.py
+++ mlos_bench/mlos_bench/os_env.py
@@ -3,7 +3,14 @@
 # Licensed under the MIT License.
 #
 """
-Simple abstraction for the OS environment variables.
+Simple platform agnostic abstraction for the OS environment variables.
+Meant as a replacement for os.environ vs nt.environ.
+
+Example
+-------
+from mlos_bench.os_env import environ
+environ['FOO'] = 'bar'
+environ.get('PWD')
 """
 
 import os
@@ -11,13 +18,11 @@ import sys
 
 
 if sys.platform == 'win32':
-    import nt
+    import nt   # type: ignore[import-not-found]
 
 
 # Handle case sensitivity differences between platforms.
 # https://stackoverflow.com/a/19023293
 environ: os._Environ[str] = nt.environ if sys.platform == 'win32' else os.environ
-# alias
-os_environ = environ
 
-__all__ = ['environ', 'os_environ']
+__all__ = ['environ']
