2d8aeaba040c0b7473d1b67db0dc58552f073b2f
Brian Kroth <bpkroth@microsoft.com>
2024-01-08 18:31:34 +0000

    comments

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -14,4 +14,5 @@ cd "$scriptdir/.."
 
 set -x
 # Example usage: "./update-version.sh --dry-run patch" to bump v0.0.4 -> v0.0.5, for instance.
+# Or:            "./update-version 0.2.0", to set the version to 0.2.0 explicitly, for instance.
 conda run -n ${CONDA_ENV_NAME:-mlos} bumpversion --verbose $*
