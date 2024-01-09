f1e55d6b46b66c45aa3e658e5be2990a8dd792a0
Brian Kroth <bpkroth@microsoft.com>
2024-01-09 23:25:40 +0000

    update to new setup.py format

--- mlos_viz/setup.py
+++ mlos_viz/setup.py
@@ -6,13 +6,45 @@
 Setup instructions for the mlos_viz package.
 """
 
+# pylint: disable=duplicate-code
+
 from logging import warning
 from itertools import chain
 from typing import Dict, List
 
+import os
+import re
+
 from setuptools import setup, find_packages
 
 from _version import _VERSION    # pylint: disable=import-private-name
+
+
+# A simple routine to read and adjust the README.md for this module into a format
+# suitable for packaging.
+# See Also: copy-source-tree-docs.sh
+# Unfortunately we can't use that directly due to the way packaging happens inside a
+# temp directory.
+# Similarly, we can't use a utility script outside this module, so this code has to
+# be duplicated for now.
+def _get_long_desc_from_readme(base_url: str) -> dict:
+    pkg_dir = os.path.dirname(__file__)
+    readme_path = os.path.join(pkg_dir, 'README.md')
+    if not os.path.isfile(readme_path):
+        return {}
+    jsonc_re = re.compile(r'```jsonc')
+    link_re = re.compile(r'\]\(([^:#)]+)(#[a-zA-Z0-9_-]+)?\)')
+    with open(readme_path, mode='r', encoding='utf-8') as readme_fh:
+        lines = readme_fh.readlines()
+        # Tweak the lexers for local expansion by pygments instead of github's.
+        lines = [link_re.sub(f"]({base_url}" + r'/\1\2)', line) for line in lines]
+        # Tweak source source code links.
+        lines = [jsonc_re.sub(r'```json', line) for line in lines]
+        return {
+            'long_description': ''.join(lines),
+            'long_description_content_type': 'text/markdown',
+        }
+
 
 try:
     from setuptools_scm import get_version
@@ -42,14 +74,32 @@ setup(
     },
     install_requires=[
         'mlos-bench==' + _VERSION,
-        'dabl @ git+https://github.com/dabl/dabl@0.2.6',    # TODO: Remove in favor of latest pypi release >= 0.2.6
+        'dabl>=0.2.7',
     ],
     extras_require=extra_requires,
     author='Microsoft',
+    license='MIT',
+    **_get_long_desc_from_readme('https://github.com/microsoft/MLOS/tree/main/mlos_viz'),
     author_email='mlos-maintainers@service.microsoft.com',
     description=('MLOS Visualization Python interface for benchmark automation and optimization results.'),
-    license='MIT',
-    keywords='',
-    url='https://aka.ms/MLOS',
+    url='https://github.com/microsoft/MLOS',
+    project_urls={
+        'Documentation': 'https://microsoft.github.io/MLOS',
+        'Package Source': 'https://github.com/microsoft/MLOS/tree/main/mlos_viz/',
+    },
     python_requires='>=3.8',
+    keywords=[
+        'autotuning',
+        'benchmarking',
+        'optimization',
+        'systems',
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
 )
