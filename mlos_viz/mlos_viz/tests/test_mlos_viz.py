ca1fc6838c20c9657bea3112c2ab79f78af6160f
Brian Kroth <bpkroth@microsoft.com>
2024-01-16 19:11:44 +0000

    Add test for auto type

---
+++ mlos_viz/mlos_viz/tests/test_mlos_viz.py
@@ -0,0 +1,14 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+"""
+Unit tests for mlos_viz.
+"""
+
+from mlos_viz import MlosVizMethod
+
+
+def test_auto_method_type() -> None:
+    """Ensure the AUTO method is what we expect."""
+    assert MlosVizMethod.AUTO == MlosVizMethod.DABL
