34742d054b77989c662f82bb5af5c9174f264ab3
Brian Kroth <bpkroth@microsoft.com>
2024-01-16 19:12:44 +0000

    wip

--- mlos_viz/mlos_viz/tests/test_dabl_plot.py
+++ mlos_viz/mlos_viz/tests/test_dabl_plot.py
@@ -9,5 +9,5 @@ Unit tests for mlos_viz.dabl.plot.
 
 def test_placeholder() -> None:
     """Placeholder test."""
-    # TODO: Remove this and implement real tests for mlos_viz.plot()
+    # TODO:
     # See Also: https://stackoverflow.com/questions/27948126/how-can-i-write-unit-tests-against-code-that-uses-matplotlib
