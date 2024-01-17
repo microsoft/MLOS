98ce9077d3d3fa98f3212318a3a6de0603c06e3a
Brian Kroth <bpkroth@microsoft.com>
2024-01-17 22:49:30 +0000

    improve test

--- mlos_viz/mlos_viz/tests/test_dabl_plot.py
+++ mlos_viz/mlos_viz/tests/test_dabl_plot.py
@@ -6,10 +6,18 @@
 Unit tests for mlos_viz.dabl.plot.
 """
 
+import warnings
+
 from mlos_bench.storage.base_experiment_data import ExperimentData
-from mlos_viz.dabl import plot as dabl_plot
+
+from mlos_viz import dabl
 
 
 def test_dabl_plot(exp_data: ExperimentData) -> None:
     """Tests plotting via dabl."""
-    dabl_plot(exp_data)
+    # For now, just ensure that no errors are thrown.
+    # TODO: Check that a plot was actually produced matching our specifications.
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        dabl.ignore_plotter_warnings()
+        dabl.plot(exp_data)
