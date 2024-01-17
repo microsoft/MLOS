e8cd0bf6e8a5096f09d9b3233f25c7cab06e8d0e
Brian Kroth <bpkroth@microsoft.com>
2024-01-17 22:51:11 +0000

    wip: add more test implementations

--- mlos_viz/mlos_viz/tests/test_mlos_viz.py
+++ mlos_viz/mlos_viz/tests/test_mlos_viz.py
@@ -6,7 +6,11 @@
 Unit tests for mlos_viz.
 """
 
-from mlos_viz import MlosVizMethod
+import warnings
+
+from mlos_bench.storage.base_experiment_data import ExperimentData
+
+from mlos_viz import MlosVizMethod, ignore_plotter_warnings, plot
 
 
 def test_auto_method_type() -> None:
@@ -14,5 +18,11 @@ def test_auto_method_type() -> None:
     assert MlosVizMethod.AUTO.value == MlosVizMethod.DABL.value
 
 
-def test_plot() -> None:
-    raise NotImplementedError("TODO")
+def test_plot(exp_data: ExperimentData) -> None:
+    """Tests plotting via dabl."""
+    # For now, just ensure that no errors are thrown.
+    # TODO: Check that a plot was actually produced matching our specifications.
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        ignore_plotter_warnings()
+        plot(exp_data)
