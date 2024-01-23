c786a25fe2bffb12195f034d6fc2a64f64e78471
Brian Kroth <bpkroth@users.noreply.github.com>
2024-01-22 20:42:33 -0600

    Remove TunableGroups from Storage classes and add TrialData tests (#644)
    Work split out from #633
    
    - Reorgs the unit test fixtures for reuse by mlos_viz (future PR)
    - Removes TunablesGroups from Storage classes. Currently unnecessary,
    belongs in Experiment, and causes inconsistencies with ExperimentData
    interactions.
    - Adjusts the initialization code to match.
    
    ---------
    
    Co-authored-by: Sergiy Matusevych <sergiym@microsoft.com>

--- mlos_bench/mlos_bench/tests/storage/conftest.py
+++ mlos_bench/mlos_bench/tests/storage/conftest.py
@@ -3,44 +3,17 @@
 # Licensed under the MIT License.
 #
 """
-Test fixtures for mlos_bench storage.
+Export test fixtures for mlos_bench storage.
 """
 
-import pytest
+import mlos_bench.tests.storage.sql.fixtures as sql_storage_fixtures
 
-from mlos_bench.tunables.tunable_groups import TunableGroups
-from mlos_bench.storage.base_storage import Storage
-from mlos_bench.storage.sql.storage import SqlStorage
+# NOTE: For future storage implementation additions, we can refactor this to use
+# lazy_fixture and parameterize the tests across fixtures but keep the test code the
+# same.
 
-# pylint: disable=redefined-outer-name
-
-
-@pytest.fixture
-def storage_memory_sql(tunable_groups: TunableGroups) -> SqlStorage:
-    """
-    Test fixture for in-memory SQLite3 storage.
-    """
-    return SqlStorage(
-        tunables=tunable_groups,
-        service=None,
-        config={
-            "drivername": "sqlite",
-            "database": ":memory:",
-        }
-    )
-
-
-@pytest.fixture
-def exp_storage_memory_sql(storage_memory_sql: Storage) -> SqlStorage.Experiment:
-    """
-    Test fixture for Experiment using in-memory SQLite3 storage.
-    """
-    # pylint: disable=unnecessary-dunder-call
-    return storage_memory_sql.experiment(
-        experiment_id="Test-001",
-        trial_id=1,
-        root_env_config="environment.jsonc",
-        description="pytest experiment",
-        opt_target="score",
-        opt_direction="min",
-    ).__enter__()
+# Expose some of those as local names so they can be picked up as fixtures by pytest.
+storage = sql_storage_fixtures.storage
+exp_storage = sql_storage_fixtures.exp_storage
+exp_storage_with_trials = sql_storage_fixtures.exp_storage_with_trials
+exp_data = sql_storage_fixtures.exp_data
