56e84fdc6339bf020e5d6961f7f37ea6b321f24b
Brian Kroth <bpkroth@users.noreply.github.com>
2024-01-26 16:52:54 -0600

    Add `default_tunable_config_id` property to `ExperimentData` (#650)
    Splitting work out from #633.
    
    Having an easy way to identify what is the "default" config group for an
    Experiment makes analysis of improvements easier.
    
    One challenge with this is that we don't currently strictly enforce
    non-changes between resumptions of an Experiment, so it's currently
    possible for the user to change the tunables config and adjust what the
    "default".
    
    Moreover, in the past we didn't store what the default config was in the
    DB.
    
    By convention we take the first one for now as a fallback.

--- mlos_bench/mlos_bench/storage/base_experiment_data.py
+++ mlos_bench/mlos_bench/storage/base_experiment_data.py
@@ -7,7 +7,8 @@ Base interface for accessing the stored benchmark experiment data.
 """
 
 from abc import ABCMeta, abstractmethod
-from typing import Dict, Tuple, TYPE_CHECKING
+from distutils.util import strtobool    # pylint: disable=deprecated-module
+from typing import Dict, Optional, Tuple, TYPE_CHECKING
 
 import pandas
 
@@ -118,6 +119,33 @@ class ExperimentData(metaclass=ABCMeta):
         trials : Dict[int, TunableConfigTrialGroupData]
             A dictionary of the trials' data, keyed by (tunable) by config id.
         """
+
+    @property
+    def default_tunable_config_id(self) -> Optional[int]:
+        """
+        Retrieves the (tunable) config id for the default tunable values for this experiment.
+
+        Note: this is by *default* the first trial executed for this experiment.
+        However, it is currently possible that the user changed the tunables config
+        in between resumptions of an experiment.
+
+        Returns
+        -------
+        int
+        """
+        # Note: this implementation is quite inefficient and may be better
+        # reimplemented by subclasses.
+
+        # Check to see if we included it in trial metadata.
+        trials_items = sorted(self.trials.items())
+        if not trials_items:
+            return None
+        for (_trial_id, trial) in trials_items:
+            # Take the first config id marked as "defaults" when it was instantiated.
+            if strtobool(str(trial.metadata_dict.get('is_defaults', False))):
+                return trial.tunable_config_id
+        # Fallback (min trial_id)
+        return trials_items[0][1].tunable_config_id
 
     @property
     @abstractmethod
