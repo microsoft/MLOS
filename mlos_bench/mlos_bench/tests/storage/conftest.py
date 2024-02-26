32f57a4f78eb0da67ad0401a50bcba9142f9652b
Brian Kroth <bpkroth@microsoft.com>
2024-02-26 16:40:11 +0000

    Add tests for config data retrieval for experiments with no tunables

--- mlos_bench/mlos_bench/tests/storage/conftest.py
+++ mlos_bench/mlos_bench/tests/storage/conftest.py
@@ -15,9 +15,11 @@ import mlos_bench.tests.storage.sql.fixtures as sql_storage_fixtures
 # Expose some of those as local names so they can be picked up as fixtures by pytest.
 storage = sql_storage_fixtures.storage
 exp_storage = sql_storage_fixtures.exp_storage
-exp_storage_no_tunables = sql_storage_fixtures.exp_storage_no_tunables
+exp_no_tunables_storage = sql_storage_fixtures.exp_no_tunables_storage
 mixed_numerics_exp_storage = sql_storage_fixtures.mixed_numerics_exp_storage
 exp_storage_with_trials = sql_storage_fixtures.exp_storage_with_trials
+exp_no_tunables_storage_with_trials = sql_storage_fixtures.exp_no_tunables_storage_with_trials
 mixed_numerics_exp_storage_with_trials = sql_storage_fixtures.mixed_numerics_exp_storage_with_trials
 exp_data = sql_storage_fixtures.exp_data
+exp_no_tunables_data = sql_storage_fixtures.exp_no_tunables_data
 mixed_numerics_exp_data = sql_storage_fixtures.mixed_numerics_exp_data
--- mlos_bench/mlos_bench/tests/storage/trial_config_test.py
+++ mlos_bench/mlos_bench/tests/storage/trial_config_test.py
@@ -64,14 +64,14 @@ def test_exp_trial_configs(exp_storage: Storage.Experiment,
     assert set(pending_ids) == {trials1[0].tunable_config_id, trials2[0].tunable_config_id}
 
 
-def test_exp_trial_no_config(exp_storage_no_tunables: Storage.Experiment) -> None:
+def test_exp_trial_no_config(exp_no_tunables_storage: Storage.Experiment) -> None:
     """
     Schedule a trial that has an empty tunable groups config.
     """
     empty_config: dict = {}
     tunable_groups = TunableGroups(config=empty_config)
-    trial = exp_storage_no_tunables.new_trial(tunable_groups, config=empty_config)
-    (pending,) = exp_storage_no_tunables.pending_trials(datetime.utcnow(), running=True)
+    trial = exp_no_tunables_storage.new_trial(tunable_groups, config=empty_config)
+    (pending,) = exp_no_tunables_storage.pending_trials(datetime.utcnow(), running=True)
     assert pending.trial_id == trial.trial_id
     assert pending.tunables == tunable_groups
     assert pending.config() == {
