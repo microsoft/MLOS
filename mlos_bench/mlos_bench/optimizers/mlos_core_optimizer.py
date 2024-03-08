dd788e4aaaddcccd635895ef161cc498e4f0ab5f
Sergiy Matusevych <sergiym@microsoft.com>
2024-03-07 18:01:47 -0800

    move some common functionality like tracking the iterations and best scores to the base class

--- mlos_bench/mlos_bench/optimizers/mlos_core_optimizer.py
+++ mlos_bench/mlos_bench/optimizers/mlos_core_optimizer.py
@@ -103,7 +103,7 @@ class MlosCoreOptimizer(Optimizer):
             df_configs = df_configs[df_status_completed]
             df_scores = df_scores[df_status_completed]
         self._opt.register(df_configs, df_scores)
-        if not is_warm_up:
+        if is_warm_up:
             self._iter += len(df_scores)
         if _LOG.isEnabledFor(logging.DEBUG):
             (score, _) = self.get_best_observation()
@@ -169,7 +169,6 @@ class MlosCoreOptimizer(Optimizer):
             df_config = self._to_df([tunables.get_param_values()])
             _LOG.debug("Score: %s Dataframe:\n%s", score, df_config)
             self._opt.register(df_config, pd.Series([score], dtype=float))
-        self._iter += 1
         return score
 
     def get_best_observation(self) -> Union[Tuple[float, TunableGroups], Tuple[None, None]]:
---
+++ mlos_bench/mlos_bench/optimizers/track_best_optimizer.py
@@ -0,0 +1,52 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+"""
+Mock optimizer for mlos_bench.
+"""
+
+import logging
+
+from abc import abstractmethod
+from typing import Optional, Tuple, Union
+
+from mlos_bench.environments.status import Status
+from mlos_bench.tunables.tunable_groups import TunableGroups
+
+from mlos_bench.optimizers.base_optimizer import Optimizer
+from mlos_bench.services.base_service import Service
+
+_LOG = logging.getLogger(__name__)
+
+
+class TrackBestOptimizer(Optimizer):
+    """
+    Base Optimizer class that keeps track of the best score and configuration.
+    """
+
+    def __init__(self,
+                 tunables: TunableGroups,
+                 config: dict,
+                 global_config: Optional[dict] = None,
+                 service: Optional[Service] = None):
+        super().__init__(tunables, config, global_config, service)
+        self._best_config: Optional[TunableGroups] = None
+        self._best_score: Optional[float] = None
+
+    @abstractmethod
+    def register(self, tunables: TunableGroups, status: Status,
+                 score: Optional[Union[float, dict]] = None) -> Optional[float]:
+        registered_score = super().register(tunables, status, score)
+        if status.is_succeeded() and (
+            self._best_score is None or (registered_score is not None and registered_score < self._best_score)
+        ):
+            self._best_score = registered_score
+            self._best_config = tunables.copy()
+        return registered_score
+
+    def get_best_observation(self) -> Union[Tuple[float, TunableGroups], Tuple[None, None]]:
+        if self._best_score is None:
+            return (None, None)
+        assert self._best_config is not None
+        return (self._best_score * self._opt_sign, self._best_config)
