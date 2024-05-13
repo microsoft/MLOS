a6c5a9dcacef8267c9a277436f6d1952d924ad7d
Sergiy Matusevych <sergiym@microsoft.com>
2024-05-13 14:45:51 -0700

    minimize diff with master

--- mlos_core/mlos_core/optimizers/random_optimizer.py
+++ mlos_core/mlos_core/optimizers/random_optimizer.py
@@ -61,6 +61,7 @@ class RandomOptimizer(BaseOptimizer):
             Pandas dataframe with a single row. Column names are the parameter names.
         """
         if context is not None:
+            # not sure how that works here?
             warn(f"Not Implemented: Ignoring context {list(context.columns)}", UserWarning)
         return pd.DataFrame(dict(self.optimizer_parameter_space.sample_configuration()), index=[0])
 
