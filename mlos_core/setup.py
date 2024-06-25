42ce8c015dd3b4d8c698f633a2fbde4fdd2e8ce0
Brian Kroth <bpkroth@microsoft.com>
2024-06-25 22:32:16 +0000

    Temporarily avoid broken numpy dependency

--- mlos_core/setup.py
+++ mlos_core/setup.py
@@ -88,7 +88,7 @@ setup(
         'scikit-learn>=1.2',
         'joblib>=1.1.1',        # CVE-2022-21797: scikit-learn dependency, addressed in 1.2.0dev0, which isn't currently released
         'scipy>=1.3.2',
-        'numpy>=1.24',
+        'numpy>=1.24', 'numpy<2.0.0',   # FIXME: https://github.com/numpy/numpy/issues/26710
         'pandas >= 2.2.0;python_version>="3.9"', 'Bottleneck > 1.3.5;python_version>="3.9"',
         'pandas >= 1.0.3;python_version<"3.9"',
         'ConfigSpace>=0.7.1',
