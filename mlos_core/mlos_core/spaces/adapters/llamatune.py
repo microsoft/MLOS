b4d50079d688e5b35acab668fdd09c9f750ad03a
Brian Kroth <bpkroth@microsoft.com>
2024-07-22 21:17:35 +0000

    apply the q_scaler

--- mlos_core/mlos_core/spaces/adapters/llamatune.py
+++ mlos_core/mlos_core/spaces/adapters/llamatune.py
@@ -141,6 +141,10 @@ class LlamaTuneAdapter(BaseSpaceAdapter):  # pylint: disable=too-many-instance-a
                 vector = self._config_scaler.inverse_transform([config_vector])[0]
                 # FIXME: value 3 is outside the range?
                 target_config_vector = self._pinv_matrix.dot(vector)
+                if self._q_scaler is not None:
+                    target_config_vector = self._q_scaler.inverse_transform(
+                        [target_config_vector]
+                    )[0]
                 target_config = ConfigSpace.Configuration(
                     self.target_parameter_space,
                     vector=target_config_vector,
