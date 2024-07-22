80344584e310ada45f80a07bc6278a213c7d972b
Sergiy Matusevych <sergiym@microsoft.com>
2024-07-22 09:51:20 -0700

    Fix mypy 1.11 warnings (#809)
    Mypy got updated to v 1.11 recently
    We need these mypy fixes to make other PRs pass the CI tests

--- mlos_core/mlos_core/tests/optimizers/optimizer_test.py
+++ mlos_core/mlos_core/tests/optimizers/optimizer_test.py
@@ -157,7 +157,7 @@ def test_basic_interface_toy_problem(
 def test_concrete_optimizer_type(optimizer_type: OptimizerType) -> None:
     """Test that all optimizer types are listed in the ConcreteOptimizer constraints."""
     # pylint: disable=no-member
-    assert optimizer_type.value in ConcreteOptimizer.__constraints__  # type: ignore[attr-defined]
+    assert optimizer_type.value in ConcreteOptimizer.__constraints__
 
 
 @pytest.mark.parametrize(
