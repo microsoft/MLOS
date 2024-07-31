852f68545f83671ed89f20cca8bd467e21f8abae
Brian Kroth <bpkroth@users.noreply.github.com>
2024-07-31 14:12:07 -0500

    Update mlos_core/setup.py

--- mlos_core/setup.py
+++ mlos_core/setup.py
@@ -69,7 +69,8 @@ def _get_long_desc_from_readme(base_url: str) -> dict:
 
 extra_requires: Dict[str, List[str]] = {  # pylint: disable=consider-using-namedtuple-or-dataclass
     "flaml": ["flaml[blendsearch]"],
-    "smac": ["smac>=2.2.0"],  # NOTE: Major refactoring on SMAC starting from v2.0.0
+    # NOTE: Major refactoring on SMAC and ConfigSpace v1.0 starting from v2.2
+    "smac": ["smac>=2.2.0"],
 }
 
 # construct special 'full' extra that adds requirements for all built-in
