202dcc6127a4b89999ec29d7d27a37578b8ab860
Brian Kroth <bpkroth@microsoft.com>
2024-11-12 18:48:33 +0000

    napolean tweaks

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -77,9 +77,9 @@ extensions = [
 autodoc_typehints = "both"  # signature and description
 
 napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
+napoleon_include_init_with_doc = True
 napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = False
+napoleon_include_special_with_doc = True
 napoleon_use_admonition_for_examples = False
 napoleon_use_admonition_for_notes = False
 napoleon_use_admonition_for_references = False
