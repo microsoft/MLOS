df08c2ee84d54536b0e6138e4e26645fe5b6c1f8
Brian Kroth <bpkroth@microsoft.com>
2024-11-14 19:09:48 +0000

    some more docs about environments

--- mlos_bench/mlos_bench/environments/__init__.py
+++ mlos_bench/mlos_bench/environments/__init__.py
@@ -4,16 +4,26 @@
 #
 """Tunable Environments for mlos_bench.
 
-Environments are classes that represent an execution setting (environment) for
+Environments are classes that represent an execution setting (i.e., environment) for
 running a benchmark or tuning process.
 
-An Environment goes through a series of phases (e.g.,
+For instance, a :py:class:`~.LocalEnv` represents a local execution environment, a
+:py:class:`~.RemoteEnv` represents a remote execution environment, a
+:py:class:`~mlos_bench.environments.remote.vm_env.VMEnv` represents a virtual
+machine, etc.
+
+An Environment goes through a series of *phases* (e.g.,
 :py:meth:`~.Environment.setup`, :py:meth:`~.Environment.run`,
 :py:meth:`~.Environment.teardown`, etc.) that can be used to prepare a VM, workload,
 etc.; run a benchmark, script, etc.; and clean up afterwards.
+Often, what these phases do (e.g., what commands to execute) will depend on the
+specific Environment and the configs that Environment was loaded with.
+This lets Environments be very flexible in what they can accomplish.
 
 Environments can be stacked together with the :py:class:`.CompositeEnv` class to
-represent complex setups (e.g., an appication running on a VM).
+represent complex setups (e.g., an appication running on a remote VM with a
+benchmark running from a local machine).
+
 See below for the set of Environments currently available in this package.
 
 Note that additional ones can also be created by extending the base
@@ -32,7 +42,13 @@ environment can use to configure itself.
 
 At a python level, this happens by passing a
 :py:meth:`~mlos_bench.tunables.tunable_groups.TunableGroups` object to the
-``tunable_groups`` parameter of the :py:class:`~.Environment` constructor.
+``tunable_groups`` parameter of the :py:class:`~.Environment` constructor, but that
+is typically handled by the
+:py:meth:`~mlos_bench.services.config_persistence.ConfigPersistence.load_environment`
+method of the
+:py:meth:`~mlos_bench.services.config_persistence.ConfigPersistenceService` invoked
+by the ``mlos_bench`` command line tool's :py:class:`mlos_bench.loader.Loader`
+class.
 
 In the typical json user level configs, this is specified in the
 ``include_tunables`` section of the Environment config to include the
@@ -56,7 +72,11 @@ necessary support to perform the actions that environment needs for each of its
 phases depending upon where its being deployed (e.g., local machine, remote machine,
 cloud provider VM, etc.)
 
-TODO: Add more details here.
+Although this can be done in the Environment config directly with the
+``include_services`` key, it is often more useful to do it in the global or
+:py:mod:`cli config <mlos_bench.config>` to allow for the same Environment to be
+used in different settings (e.g., local machine, SSH accessible machine, Azure VM,
+etc.) without having to change the Environment config.
 
 Examples
 --------
@@ -64,11 +84,13 @@ While this documentation is generated from the source code and is intended to be
 useful reference on the internal details, most users will be more interested in
 generating json configs to be used with the ``mlos_bench`` command line tool.
 
-For a working example please see the `test_local_env_bench.jsonc
+For a simple working user oriented example please see the `test_local_env_bench.jsonc
 <https://github.com/microsoft/MLOS/blob/main/mlos_bench/mlos_bench/tests/config/environments/local/test_local_env.jsonc>`_
 file or other examples in the source tree linked below.
 
-TODO: Add examples here.
+For more developer oriented examples please see the `mlos_bench/tests/environments
+<https://github.com/microsoft/MLOS/blob/main/mlos_bench/mlos_bench/tests/>`_
+directory in the source tree.
 
 See Also
 --------
