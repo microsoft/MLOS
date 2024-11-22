726d00b5414a283e10711a1fcacbd26fd99c74dd
Brian Kroth <bpkroth@microsoft.com>
2024-11-22 20:28:42 +0000

    start removing those type aliases for now

--- mlos_core/mlos_core/data_classes.py
+++ mlos_core/mlos_core/data_classes.py
@@ -5,26 +5,12 @@
 """Data classes for mlos_core used to pass around configurations, observations, and
 suggestions.
 """
-from typing import Any, Iterator, Iterable, Optional, TypeAlias
+from typing import Any, Iterator, Iterable, Optional
 
 import pandas as pd
 from ConfigSpace import Configuration, ConfigurationSpace
 
 from mlos_core.util import compare_optional_dataframe, compare_optional_series
-
-Config: TypeAlias = pd.Series
-"""Data type for a configuration instance."""
-
-Score: TypeAlias = pd.Series
-"""Data type for a score of a given configuration run (trial)."""
-
-Context: TypeAlias = pd.Series
-"""Data type for the context in which a configuration was evaluated or the
-suggesttion is requested."""
-
-Metadata: TypeAlias = pd.Series
-"""Data type for additional metadata about the configuration suggestion to be fed
-back to the underlying optimizer."""
 
 
 class Observation:
@@ -37,10 +23,10 @@ class Observation:
         The configuration observed.
     score : pd.Series
         The score metrics observed.
-    context : Context
+    context : Optional[pd.Series]
         The context in which the configuration was evaluated.
         Not Yet Implemented.
-    metadata: Metadata
+    metadata: Optional[pd.Series]
         The metadata in which the configuration was evaluated
     """
 
