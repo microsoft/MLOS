979fc530c8105461dc6a2de64049dcf15989a9ed
Brian Kroth <bpkroth@microsoft.com>
2024-12-05 22:39:32 +0000

    comments

--- mlos_core/mlos_core/spaces/__init__.py
+++ mlos_core/mlos_core/spaces/__init__.py
@@ -2,4 +2,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 #
-"""Space adapters and converters."""
+"""Space adapters and converters.
+
+See Also
+--------
+:py:mod:`mlos_core` : for an overview of configuration spaces in ``mlos_core``.
+"""
