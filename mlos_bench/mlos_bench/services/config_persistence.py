52e6d4a6bca3f0e9d98692ec531c68b25c66df3b
Brian Kroth <bpkroth@microsoft.com>
2024-12-06 21:22:42 +0000

    format

--- mlos_bench/mlos_bench/services/config_persistence.py
+++ mlos_bench/mlos_bench/services/config_persistence.py
@@ -58,8 +58,9 @@ class ConfigPersistenceService(Service, SupportsConfigLoading):
     """
 
     BUILTIN_CONFIG_PATH = str(files("mlos_bench.config").joinpath("")).replace("\\", "/")
-    """A calculated path to the built-in configuration files shipped with the
-    mlos_bench package."""
+    """A calculated path to the built-in configuration files shipped with the mlos_bench
+    package.
+    """
 
     def __init__(
         self,
