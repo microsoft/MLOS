0a94138a2d7ca4f692bc6bf8236ab42fcec21e9c
Brian Kroth <bpkroth@users.noreply.github.com>
2024-12-18 10:17:51 -0600

    CI fixups (#900)
    # Pull Request
    
    ## Title
    
    Some fixups to account for new versions of matplotlib so that CI runs
    again.
    
    ---
    
    ## Description
    
    - Pulls in a new version of dabl with some fixes:
      https://github.com/dabl/dabl/pull/353
    - Adds an ignore for a seaborn issue:
      https://github.com/mwaskom/seaborn/issues/3804
    - Adjusts the python unit test debugger config to remove a deprecated
    extension:
      https://code.visualstudio.com/docs/python/testing#_debug-tests
    
    ---
    
    ## Type of Change
    
    - 🛠️ Bug fix
    
    ---

--- mlos_viz/mlos_viz/base.py
+++ mlos_viz/mlos_viz/base.py
@@ -46,6 +46,16 @@ def ignore_plotter_warnings() -> None:
             module="seaborn",  # but actually comes from pandas
             message="is_categorical_dtype is deprecated and will be removed in a future version.",
         )
+    # See Also: https://github.com/mwaskom/seaborn/issues/3804
+    warnings.filterwarnings(
+        "ignore",
+        category=PendingDeprecationWarning,
+        module="seaborn",  # but actually comes from matplotlib
+        message=(
+            "vert: bool will be deprecated in a future version. "
+            "Use orientation: {'vertical', 'horizontal'} instead."
+        ),
+    )
 
 
 def _add_groupby_desc_column(
