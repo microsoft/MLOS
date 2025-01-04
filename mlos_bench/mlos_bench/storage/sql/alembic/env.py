7494ed4fd24e609bf237bc9cc5376fe660ecb2f5
Brian Kroth <bpkroth@microsoft.com>
2025-01-04 00:27:47 +0000

    lint

--- mlos_bench/mlos_bench/storage/sql/alembic/env.py
+++ mlos_bench/mlos_bench/storage/sql/alembic/env.py
@@ -2,10 +2,15 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 #
+"""Alembic environment script."""
+# pylint: disable=no-member
+
 from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
+
+from mlos_bench.storage.sql.schema import DbSchema
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,10 +23,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from mlos_bench.storage.sql.schema import DbSchema
-
 target_metadata = DbSchema(engine=None).meta
-# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -30,16 +32,14 @@ target_metadata = DbSchema(engine=None).meta
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+    """
+    Run migrations in 'offline' mode.
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
+    This configures the context with just a URL and not an Engine, though an Engine is
+    acceptable here as well.  By skipping the Engine creation we don't even need a DBAPI
+    to be available.
 
-    Calls to context.execute() here emit the given string to the
-    script output.
-
+    Calls to context.execute() here emit the given string to the script output.
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -54,11 +54,11 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    """
+    Run migrations in 'online' mode.
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
+    In this scenario we need to create an Engine and associate a connection with the
+    context.
     """
     connectable = config.attributes.get("connection", None)
 
@@ -66,7 +66,7 @@ def run_migrations_online() -> None:
         # only create Engine if we don't have a Connection
         # from the outside
         connectable = engine_from_config(
-            config.get_section(config.config_ini_section),
+            config.get_section(config.config_ini_section) or {},
             prefix="sqlalchemy.",
             poolclass=pool.NullPool,
         )
