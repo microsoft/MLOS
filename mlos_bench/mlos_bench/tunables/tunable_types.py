e98cba78408df61114d4f74229cbc502b3c357b6
Brian Kroth <bpkroth@microsoft.com>
2025-01-20 17:20:24 -0600

    more docstring fixups

--- mlos_bench/mlos_bench/tunables/tunable_types.py
+++ mlos_bench/mlos_bench/tunables/tunable_types.py
@@ -2,7 +2,15 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 #
-"""Helper types for :py:class:`~mlos_bench.tunables.tunable.Tunable`."""
+"""
+Helper types for :py:class:`~mlos_bench.tunables.tunable.Tunable`.
+
+The main class of interest to most users in this module is :py:class:`.TunableDict`,
+which provides the typed conversions from a JSON config to a config used for
+creating a :py:class:`~mlos_bench.tunables.tunable.Tunable`.
+
+The other types are mostly used for type checking and documentation purposes.
+"""
 
 # NOTE: pydoctest doesn't scan variable docstrings so we put the examples in the
 # Tunable class docstrings.
@@ -10,42 +18,41 @@
 # the Tunable class itself.
 
 from collections.abc import Sequence
-from typing import Any, Literal, TypedDict
+from typing import TYPE_CHECKING, Any, Literal, TypeAlias, TypedDict
 
-import json5 as json
+if TYPE_CHECKING:
+    # Used to allow for shorter docstring references.
+    from mlos_bench.tunables.tunable import Tunable
 
-TunableValue = int | float | str | None
-"""A :py:class:`TypeAlias` for a :py:class:`mlos_bench.tunables.tunable.Tunable`
-parameter value."""
+TunableValue: TypeAlias = int | float | str | None
+"""A :py:class:`TypeAlias` for a :py:class:`~.Tunable` parameter value."""
 
-TunableValueType = type[int] | type[float] | type[str]
-"""A :py:class:`TypeAlias` for :py:class:`mlos_bench.tunables.tunable.Tunable` value
-:py:attr:`data type <mlos_bench.tunables.tunable.Tunable.dtype>`.
+TunableValueType: TypeAlias = type[int] | type[float] | type[str]
+"""A :py:class:`TypeAlias` for :py:class:`~.Tunable` value
+:py:attr:`data type <.Tunable.dtype>`.
 
 See Also
 --------
-:py:attr:`Tunable.dtype <mlos_bench.tunables.tunable.Tunable.dtype>` : Example of
-    accepted types.
+:py:attr:`Tunable.dtype <.Tunable.dtype>` : Example of accepted types.
 """
 
 TunableValueTypeTuple = (int, float, str, type(None))
 """
-Tunable value type tuple.
+Tunable value ``type`` tuple.
 
 Notes
 -----
-For checking with :py:func:`isinstance`.
+For checking whether a param is a :py:data:`.TunableValue` with
+:py:func:`isinstance`.
 """
 
 TunableValueTypeName = Literal["int", "float", "categorical"]
 """
-The accepted string names of a :py:class:`mlos_bench.tunables.tunable.Tunable` value
-:py:attr:`~mlos_bench.tunables.tunable.Tunable.type`.
+The accepted string names of a :py:class:`~.Tunable` value :py:attr:`~.Tunable.type`.
 
 See Also
 --------
-:py:attr:`Tunable.type <mlos_bench.tunables.tunable.Tunable>` : Example of accepted
-    type names.
+:py:attr:`Tunable.type <.Tunable.type>` : Example of accepted type names.
 """
 
 TUNABLE_DTYPE: dict[TunableValueTypeName, TunableValueType] = {
@@ -54,13 +61,11 @@ TUNABLE_DTYPE: dict[TunableValueTypeName, TunableValueType] = {
     "categorical": str,
 }
 """
-Maps :py:class:`~mlos_bench.tunables.tunable.Tunable` types to their corresponding
-Python data types by name.
+Maps :py:class:`~.Tunable` types to their corresponding Python data types by name.
 
 See Also
 --------
-:py:attr:`Tunable.dtype <mlos_bench.tunables.tunable.Tunable.dtype>` : Example of
-    type mappings.
+:py:attr:`Tunable.dtype <.Tunable.dtype>` : Example of type mappings.
 """
 
 TunableValuesDict = dict[str, TunableValue]
@@ -68,20 +73,19 @@ TunableValuesDict = dict[str, TunableValue]
 
 DistributionName = Literal["uniform", "normal", "beta"]
 """
-The :py:class:`~mlos_bench.tunables.tunable.Tunable` value
-:py:attr:`~mlos_bench.tunables.tunable.Tunable.distribution` type names.
+The :py:attr:`~.Tunable.distribution` type names for a :py:class:`~.Tunable` value.
 
 See Also
 --------
-:py:attr:`Tunable.distribution <mlos_bench.tunables.tunable.Tunable.distribution>` : Example
-    of accepted distribution names.
+:py:attr:`Tunable.distribution <.Tunable.distribution>` : Example of accepted
+    distribution names.
 """
 
 
 class DistributionDictOpt(TypedDict, total=False):  # total=False allows for optional fields
     """
-    A TypedDict for a :py:class:`mlos_bench.tunables.tunable.Tunable` parameter's
-    optional :py:attr:`~mlos_bench.tunables.tunable.Tunable.distribution_params` config.
+    A :py:class:`TypedDict` for a :py:class:`~.Tunable` parameter's optional
+    :py:attr:`~.Tunable.distribution_params` config.
 
     Mostly used for type checking. These are the types expected to be received from
     the json config.
@@ -89,38 +93,62 @@ class DistributionDictOpt(TypedDict, total=False):  # total=False allows for opt
     Notes
     -----
     :py:class:`.DistributionDict` contains the required fields for the
-    :py:attr:`Tunable.distribution <mlos_bench.tunables.tunable.Tunable.distribution>`
-    parameter.
+    :py:attr:`Tunable.distribution <.Tunable.distribution>` parameter.
 
     See Also
     --------
-    :py:attr:`Tunable.distribution_params <mlos_bench.tunables.tunable.Tunable.distribution_params>`
+    :py:attr:`Tunable.distribution_params <.Tunable.distribution_params>` : Examples
+        of distribution parameters.
     """  # pylint: disable=line-too-long # noqa: E501
 
+    def __init__(self, *args, **kwargs):  # type: ignore # pylint: disable=useless-super-delegation
+        """.. comment: don't inherit the docstring"""
+        super().__init__(*args, **kwargs)
+
     params: dict[str, float] | None
+    """
+    The parameters for the distribution.
+
+    See Also
+    --------
+    :py:attr:`Tunable.distribution_params <.Tunable.distribution_params>` : Examples
+        of distribution parameters.
+    """  # pylint: disable=line-too-long # noqa: E501
 
 
 class DistributionDict(DistributionDictOpt):
     """
-    A TypedDict for a :py:class:`mlos_bench.tunables.tunable.Tunable` parameter's
-    required ``distribution`` config parameters.
+    A :py:class:`TypedDict` for a :py:class:`~.Tunable` parameter's required
+    :py:attr:`~.Tunable.distribution` config parameters.
 
     Mostly used for type checking. These are the types expected to be received from the
     json config.
 
     See Also
     --------
-    :py:attr:`Tunable.distribution <mlos_bench.tunables.tunable.Tunable.distribution>`
-    :py:attr:`Tunable.distribution_params <mlos_bench.tunables.tunable.Tunable.distribution_params>`
+    :py:attr:`Tunable.distribution <.Tunable.distribution>`
+    :py:attr:`Tunable.distribution_params <.Tunable.distribution_params>`
     """  # pylint: disable=line-too-long # noqa: E501
 
+    def __init__(self, *args, **kwargs):  # type: ignore # pylint: disable=useless-super-delegation
+        """.. comment: don't inherit the docstring"""
+        super().__init__(*args, **kwargs)
+
     type: DistributionName
+    """
+    The name of the distribution.
+
+    See Also
+    --------
+    :py:attr:`Tunable.distribution <.Tunable.distribution>` : Examples of
+        distribution names.
+    """
 
 
 class TunableDictOpt(TypedDict, total=False):  # total=False allows for optional fields
     """
-    A TypedDict for a :py:class:`mlos_bench.tunables.tunable.Tunable` parameter's
-    optional config parameters.
+    A :py:class:`TypedDict` for a :py:class:`~.Tunable` parameter's optional config
+    parameters.
 
     Mostly used for type checking. These are the types expected to be received from
     the json config.
@@ -128,37 +156,40 @@ class TunableDictOpt(TypedDict, total=False):  # total=False allows for optional
     Notes
     -----
     :py:class:`TunableDict` contains the required fields for the
-    :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    :py:class:`~.Tunable` parameter.
     """
+
+    def __init__(self, *args, **kwargs):  # type: ignore # pylint: disable=useless-super-delegation
+        """.. comment: don't inherit the docstring"""
+        super().__init__(*args, **kwargs)
 
     # Optional fields:
 
     description: str | None
     """
-    Description of the :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    Description of the :py:class:`~.Tunable` parameter.
 
     See Also
     --------
-    :py:attr:`Tunable.description <mlos_bench.tunables.tunable.Tunable.description>`
+    :py:attr:`Tunable.description <.Tunable.description>`
     """
 
     values: list[str | None] | None
     """
-    List of values (or categories) for a "categorical" type
-    :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    List of values (or categories) for a "categorical" type :py:class:`~.Tunable`
+    parameter.
 
     A list of values is required for "categorical" type Tunables.
 
     See Also
     --------
-    :py:attr:`Tunable.categories <mlos_bench.tunables.tunable.Tunable.categories>`
-    :py:attr:`Tunable.values <mlos_bench.tunables.tunable.Tunable.values>`
+    :py:attr:`Tunable.categories <.Tunable.categories>`
+    :py:attr:`Tunable.values <.Tunable.values>`
     """
 
     range: Sequence[int] | Sequence[float] | None
     """
-    The range of values for an "int" or "float" type
-    :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    The range of values for an "int" or "float" type :py:class:`~.Tunable` parameter.
 
     Must be a sequence of two values: ``[min, max]``.
 
@@ -166,14 +197,13 @@ class TunableDictOpt(TypedDict, total=False):  # total=False allows for optional
 
     See Also
     --------
-    :py:attr:`Tunable.range <mlos_bench.tunables.tunable.Tunable.range>`
-    :py:attr:`Tunable.values <mlos_bench.tunables.tunable.Tunable.values>`
+    :py:attr:`Tunable.range <.Tunable.range>` : Examples of ranges.
+    :py:attr:`Tunable.values <.Tunable.values>`
     """
 
     special: list[int] | list[float] | None
     """
-    List of special values for an "int" or "float" type
-    :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    List of special values for an "int" or "float" type :py:class:`~.Tunable` parameter.
 
     These are values that are considered special by the target system (e.g.,
     ``null``, ``0``, ``-1``, ``auto``, etc.) and should be sampled with higher
@@ -181,85 +211,89 @@ class TunableDictOpt(TypedDict, total=False):  # total=False allows for optional
 
     See Also
     --------
-    :py:attr:`Tunable.special <mlos_bench.tunables.tunable.Tunable.special>`
+    :py:attr:`Tunable.special <.Tunable.special>` : Examples of special values.
     """
 
     quantization_bins: int | None
     """
-    The number of quantization bins for an "int" or "float" type
-    :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    The number of quantization bins for an "int" or "float" type :py:class:`~.Tunable`
+    parameter.
 
     See Also
     --------
-    :py:attr:`Tunable.quantization_bins <mlos_bench.tunables.tunable.Tunable.quantization_bins>`
+    :py:attr:`Tunable.quantization_bins <.Tunable.quantization_bins>` : Examples of
+        quantized Tunables.
     """
 
     log: bool | None
     """
-    Whether to use log sampling for an "int" or "float" type
-    :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    Whether to use log sampling for an "int" or "float" type :py:class:`~.Tunable`
+    parameter.
 
     See Also
     --------
-    :py:attr:`Tunable.is_log <mlos_bench.tunables.tunable.Tunable.is_log>`
+    :py:attr:`Tunable.is_log <.Tunable.is_log>`
     """
 
     distribution: DistributionDict | None
     """
     Optional sampling distribution configuration for an "int" or "float" type
-    :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    :py:class:`~.Tunable` parameter.
 
     See Also
     --------
-    :py:attr:`Tunable.distribution <mlos_bench.tunables.tunable.Tunable.distribution>`
-    :py:attr:`Tunable.distribution_params <mlos_bench.tunables.tunable.Tunable.distribution_params>`
+    :py:attr:`Tunable.distribution <.Tunable.distribution>` : Examples of
+        distributions.
+    :py:attr:`Tunable.distribution_params <.Tunable.distribution_params>` : Examples
+        of distribution parameters.
     """  # pylint: disable=line-too-long # noqa: E501
 
     values_weights: list[float] | None
     """
     Optional sampling weights for the values of a "categorical" type
-    :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    :py:class:`~.Tunable` parameter.
 
     See Also
     --------
-    :py:attr:`Tunable.weights <mlos_bench.tunables.tunable.Tunable.weights>`
+    :py:attr:`Tunable.weights <.Tunable.weights>` : Examples of weighted sampling Tunables.
     """
 
     special_weights: list[float] | None
     """
     Optional sampling weights for the special values of an "int" or "float" type
-    :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    :py:class:`~.Tunable` parameter.
 
     See Also
     --------
-    :py:attr:`Tunable.weights <mlos_bench.tunables.tunable.Tunable.weights>`
+    :py:attr:`Tunable.weights <.Tunable.weights>` : Examples of weighted sampling Tunables.
     """
 
     range_weight: float | None
     """
     Optional sampling weight for the main ranges of an "int" or "float" type
-    :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    :py:class:`~.Tunable` parameter.
 
     See Also
     --------
-    :py:attr:`Tunable.range_weight <mlos_bench.tunables.tunable.Tunable.range_weight>`
+    :py:attr:`Tunable.range_weight <.Tunable.range_weight>` : Examples of weighted
+        sampling Tunables.
     """
 
     meta: dict[str, Any]
     """
-    Free form dict to store additional metadata for the
-    :py:class:`mlos_bench.tunables.tunable.Tunable` parameter (e.g., unit suffix, etc.)
+    Free form dict to store additional metadata for the :py:class:`~.Tunable` parameter
+    (e.g., unit suffix, etc.)
 
     See Also
     --------
-    :py:attr:`Tunable.meta <mlos_bench.tunables.tunable.Tunable.meta>`
+    :py:attr:`Tunable.meta <.Tunable.meta>` : Examples of Tunables with metadata.
     """
 
 
 class TunableDict(TunableDictOpt):
     """
-    A TypedDict for a :py:class:`mlos_bench.tunables.tunable.Tunable` parameter's
-    required config parameters.
+    A :py:class:`TypedDict` for a :py:class:`~.Tunable` parameter's required config
+    parameters.
 
     Mostly used for type checking. These are the types expected to be received from
     the json config.
@@ -276,26 +310,28 @@ class TunableDict(TunableDictOpt):
     {'type': 'categorical', 'default': 'a', 'values': ['a', 'b']}
     """
 
-    # TODO: Add docstrings for this fields that refer to *working* examples in the Tunable class.
+    def __init__(self, *args, **kwargs):  # type: ignore # pylint: disable=useless-super-delegation
+        """.. comment: don't inherit the docstring"""
+        super().__init__(*args, **kwargs)
 
     # Required fields
+
     type: TunableValueTypeName
     """
-    The name of the type of the :py:class:`mlos_bench.tunables.tunable.Tunable`
-    parameter.
+    The name of the type of the :py:class:`~.Tunable` parameter.
 
     See Also
     --------
-    :py:attr:`Tunable.type <mlos_bench.tunables.tunable.Tunable.type>` : Examples of type names.
+    :py:attr:`Tunable.type <.Tunable.type>` : Examples of type names.
     """
 
     default: TunableValue
     """
-    The default value of the :py:class:`mlos_bench.tunables.tunable.Tunable` parameter.
+    The default value of the :py:class:`~.Tunable` parameter.
 
     See Also
     --------
-    :py:attr:`Tunable.default <mlos_bench.tunables.tunable.Tunable.default>`
+    :py:attr:`Tunable.default <.Tunable.default>`
     """
 
 
@@ -306,7 +342,7 @@ def tunable_dict_from_dict(config: dict[str, Any]) -> TunableDict:
     Notes
     -----
     Mostly used for type checking while instantiating a
-    :py:class:`mlos_bench.tunables.tunable.Tunable` from a json config.
+    :py:class:`~.Tunable` from a json config.
 
     Parameters
     ----------
@@ -348,26 +384,3 @@ def tunable_dict_from_dict(config: dict[str, Any]) -> TunableDict:
         range_weight=config.get("range_weight"),
         meta=_meta,
     )
-
-
-def tunable_dict_from_json(json_config: str) -> TunableDict:
-    """
-    Creates a TunableDict from a json string.
-
-    Notes
-    -----
-    Just a convenient wrapper around :py:func:`json.loads` and
-    :py:func:`.tunable_dict_from_dict`.
-
-    Parameters
-    ----------
-    json_config : str
-        A json string that represents a :py:class:`.TunableDict`.
-
-    Returns
-    -------
-    TunableDict
-    """
-    config = json.loads(json_config)
-    assert isinstance(config, dict)
-    return tunable_dict_from_dict(config)
