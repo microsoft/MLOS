c143d640a8d34bbc8528db19832d9892f8a6759b
Brian Kroth <bpkroth@microsoft.com>
2025-01-20 16:27:19 -0600

    tweak

--- mlos_bench/mlos_bench/tunables/tunable.py
+++ mlos_bench/mlos_bench/tunables/tunable.py
@@ -427,6 +427,7 @@ class Tunable:  # pylint: disable=too-many-instance-attributes,too-many-public-m
         special : [int] | [float]
             A list of special values of the Tunable. Can be empty.
         """
+        assert self.is_numerical
         return self._special
 
     @property
