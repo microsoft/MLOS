218194e5a95a7d11e139785af951435cbc00b4e5
Brian Kroth <bpkroth@microsoft.com>
2025-01-28 19:42:40 +0000

    Check version explicitly

--- mlos_core/mlos_core/spaces/adapters/llamatune.py
+++ mlos_core/mlos_core/spaces/adapters/llamatune.py
@@ -14,7 +14,9 @@ See Also: `LlamaTune: Sample-Efficient DBMS Configuration Tuning
 <https://www.microsoft.com/en-us/research/publication/llamatune-sample-efficient-dbms-configuration-tuning>`_.
 """
 import os
+from importlib.metadata import version
 from typing import Any
+from packaging.version import Version
 from warnings import warn
 
 import ConfigSpace
@@ -28,6 +30,7 @@ from sklearn.preprocessing import MinMaxScaler
 from mlos_core.spaces.adapters.adapter import BaseSpaceAdapter
 from mlos_core.util import normalize_config
 
+_NUMPY_VERS = Version(version("numpy"))
 
 class LlamaTuneAdapter(BaseSpaceAdapter):  # pylint: disable=too-many-instance-attributes
     """Implementation of LlamaTune, a set of parameter space transformation techniques,
@@ -386,13 +389,11 @@ class LlamaTuneAdapter(BaseSpaceAdapter):  # pylint: disable=too-many-instance-a
                 orig_value = param.to_value(value)
                 orig_value = np.clip(orig_value, param.lower, param.upper)
 
-                try:
+                if _NUMPY_VERS >= Version("2.0"):
                     # Convert numpy types to native Python types (e.g., np.int64 to int)
                     # This was performed automatically in NumPy<2.0, but not anymore.
                     # see, https://numpy.org/doc/stable/reference/generated/numpy.can_cast.html
                     orig_value = orig_value.item()
-                except AttributeError:
-                    pass
 
             else:
                 raise NotImplementedError(
