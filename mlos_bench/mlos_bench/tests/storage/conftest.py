24ccf5a893d875d99f7b2d7df18dfc220a7e6fbe
Brian Kroth <bpkroth@microsoft.com>
2025-05-19 17:04:59 -0500

    reorg files based storage fixture

--- mlos_bench/mlos_bench/tests/storage/conftest.py
+++ mlos_bench/mlos_bench/tests/storage/conftest.py
@@ -12,6 +12,7 @@ import mlos_bench.tests.storage.sql.fixtures as sql_storage_fixtures
 
 # Expose some of those as local names so they can be picked up as fixtures by pytest.
 storage = sql_storage_fixtures.storage
+sqlite_storage = sql_storage_fixtures.sqlite_storage
 exp_storage = sql_storage_fixtures.exp_storage
 exp_no_tunables_storage = sql_storage_fixtures.exp_no_tunables_storage
 mixed_numerics_exp_storage = sql_storage_fixtures.mixed_numerics_exp_storage
