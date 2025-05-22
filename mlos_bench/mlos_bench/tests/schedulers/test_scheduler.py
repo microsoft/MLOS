981326890418e4ba170fdf647e7b5c69decd91af
Brian Kroth <bpkroth@microsoft.com>
2025-05-22 14:33:48 -0500

    stubbing out a very basic test to get started

--- mlos_bench/mlos_bench/tests/schedulers/test_scheduler.py
+++ mlos_bench/mlos_bench/tests/schedulers/test_scheduler.py
@@ -8,7 +8,9 @@ Unit tests for :py:class:`mlos_bench.schedulers` and their internals.
 
 import pytest
 import unittest.mock
+import sys
 
+from mlos_bench.environments.mock_env import MockEnv
 from mlos_bench.storage.sql.storage import SqlStorage
 from mlos_bench.schedulers.base_scheduler import Scheduler
 from mlos_bench.schedulers.sync_scheduler import SyncScheduler
@@ -36,3 +38,37 @@ def create_scheduler(
         storage=sqlite_storage,
         root_env_config="",
     )
+
+
+@pytest.mark.parametrize(
+    "scheduler_class",
+    [
+        SyncScheduler,
+    ],
+)
+@pytest.mark.skipif(
+    sys.platform == "win32",
+    reason="Skipping test on Windows - SQLite storage is not accessible in parallel tests there.",
+)
+def test_scheduler(
+    scheduler_class: type[Scheduler],
+    # fixtures:
+    trial_runners: list[TrialRunner],
+    mock_opt: MockOptimizer,
+    sqlite_storage: SqlStorage,
+) -> None:
+    """
+    Test the creation of a SyncScheduler instance.
+    """
+    scheduler = create_scheduler(
+        scheduler_class,
+        trial_runners,
+        mock_opt,
+        sqlite_storage,
+    )
+    assert isinstance(scheduler, scheduler_class)
+    assert isinstance(scheduler.trial_runners, list)
+    assert len(scheduler.trial_runners) == len(trial_runners)
+    assert isinstance(scheduler.optimizer, MockOptimizer)
+    assert isinstance(scheduler.storage, SqlStorage)
+    assert isinstance(scheduler.root_environment, MockEnv)
