63e0f88e7c9378df80ee36a351799bf086095000
Brian Kroth <bpkroth@microsoft.com>
2025-05-21 23:45:56 -0500

    remove

--- mlos_bench/mlos_bench/tests/schedulers/__init__.py
+++ mlos_bench/mlos_bench/tests/schedulers/__init__.py
@@ -3,8 +3,3 @@
 # Licensed under the MIT License.
 #
 """mlos_bench.tests.schedulers."""
-from mlos_bench.tests.schedulers.mock_scheduler import MockScheduler
-
-__all__ = [
-    "MockScheduler",
-]
