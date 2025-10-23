7fa0eafeecfc66c3678941791eb4e38227b91ec3
Brian Kroth <bpkroth@microsoft.com>
2025-10-22 20:13:58 -0500

    add messages

--- mlos_viz/mlos_viz/tests/conftest.py
+++ mlos_viz/mlos_viz/tests/conftest.py
@@ -4,6 +4,7 @@
 #
 """Export test fixtures for mlos_viz."""
 
+from logging import warning
 from pathlib import Path
 import glob
 import os
@@ -29,11 +30,14 @@ if sys.platform == "win32":
         os.environ["TCL_LIBRARY"] = str(
             Path(glob.glob(os.path.join(sys.base_prefix, "tcl", "tcl*", "init.tcl"))[0]).parent
         )
+        warning(f"""Setting TCL_LIBRARY to {os.environ["TCL_LIBRARY"]}""")
     if "TK_LIBRARY" not in os.environ:
         os.environ["TK_LIBRARY"] = str(
             Path(glob.glob(os.path.join(sys.base_prefix, "tcl", "tk*", "pkgIndex.tcl"))[0]).parent
         )
+        warning(f"""Setting TK_LIBRARY to {os.environ["TK_LIBRARY"]}""")
     if "TIX_LIBRARY" not in os.environ:
         os.environ["TIX_LIBRARY"] = str(
             Path(glob.glob(os.path.join(sys.base_prefix, "tcl", "tix*", "pkgIndex.tcl"))[0]).parent
         )
+        warning(f"""Setting TIX_LIBRARY to {os.environ["TIX_LIBRARY"]}""")
