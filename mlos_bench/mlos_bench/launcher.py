938d53f8d6a72b54c0c0e600b1e12d8eac1aba30
Brian Kroth <bpkroth@microsoft.com>
2025-10-29 08:28:18 -0700

    partial revert of launcher.py log format changes

--- mlos_bench/mlos_bench/launcher.py
+++ mlos_bench/mlos_bench/launcher.py
@@ -34,13 +34,16 @@ from mlos_bench.tunables.tunable_types import TunableValue
 from mlos_bench.util import try_parse_val
 
 _LOG_LEVEL = logging.INFO
-# See Also: /conftest.py, setup.cfg
-_LOG_FORMAT = (
-    "%(asctime)s.%(msecs)03d [%(process)d][%(threadName)s] "
-    "[%(filename)s:%(lineno)d %(funcName)s] "
-    "%(levelname)s %(message)s"
-)
-logging.basicConfig(level=_LOG_LEVEL, format=_LOG_FORMAT, datefmt="%Y-%m-%d %H:%M:%S")
+_LOG_FORMAT = "%(asctime)s %(filename)s:%(lineno)d %(funcName)s %(levelname)s %(message)s"
+logging.basicConfig(level=_LOG_LEVEL, format=_LOG_FORMAT)
+
+# TODO: Future PR: See Also: /conftest.py, setup.cfg
+# _LOG_FORMAT = (
+#    "%(asctime)s.%(msecs)03d [%(process)d][%(threadName)s] "
+#    "[%(filename)s:%(lineno)d %(funcName)s] "
+#    "%(levelname)s %(message)s"
+# )
+# logging.basicConfig(level=_LOG_LEVEL, format=_LOG_FORMAT, datefmt="%Y-%m-%d %H:%M:%S")
 
 _LOG = logging.getLogger(__name__)
 
